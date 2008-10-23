dd6572db2a9702834723f1fb42c4442d56ee28ba
thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2008-10-23 01:38:32 +0000

    git-svn-id: https://pymt.googlecode.com/svn/trunk@27 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    committer: thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

---
+++ examples/flowchart/task.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from flowchart import *
+from pymt import *
+
+from random import randint
+
+def createElement(style='Box',text='Flowchart Element', color=(0.5,0.5,0.3)):
+    w = None
+    x,y = randint(50, 1000), randint(50, 1000)
+    if style == 'Box':
+        w = Box(text=text, pos=(x,y),color=color)
+    elif style == 'Rhombus':
+        w = Rhombus(text=text,pos=(x,y),color=color)
+    c.add_widget(w,z=1)
+
+createElement('Box', 'Lamp doesnt work', color=(0.6,0.3,0.3))
+createElement('Rhombus', 'Lamp is plugged in?')
+createElement('Rhombus', 'Bulb burned out?')
+createElement('Box', 'plug in lamp!', color=(0.3,0.6,0.3))
+createElement('Box', 'buy new bulb!', color=(0.3,0.6,0.3))
+createElement('Box', 'buy new lamp!', color=(0.3,0.6,0.3))
+win = UIWindow(c)
+win.set_fullscreen()
+runTouchApp()

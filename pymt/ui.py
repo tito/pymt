18042cd071e4c0cf7fe330a7c419d83905e87eb1
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-13 23:00:27 +0000

    - Animation: move Animation in top of ui.py, to be used in MTWidget - MTWidget: allow to use custom animation function in add_animation
    git-svn-id: https://pymt.googlecode.com/svn/trunk@76 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/ui.py
+++ pymt/ui.py
@@ -21,6 +21,72 @@ def getWidgetByID(id):
     if _id_2_widget.has_key(id):
         return _id_2_widget[id]
 
+
+class AnimationAlpha(object):
+    """Collection of animation function, to be used with Animation object."""
+    @staticmethod
+    def ramp(value_from, value_to, length, frame):
+        return (1.0 - frame / length) * value_from  +  frame / length * value_to
+
+    @staticmethod
+    def sin(value_from, value_to, length, frame):
+        return math.sin(math.pi / 2 * (1.0 - frame / length)) * value_from  + \
+            math.sin(math.pi / 2  * (frame / length)) * value_to
+
+    @staticmethod
+    def bubble(value_from, value_to, length, frame):
+        return math.sin(math.pi * (1.0 - frame / length)) * value_from  + \
+            math.sin(math.pi  * (frame / length)) * value_to
+
+
+class Animation(object):
+    """Class to animate property over the time"""
+    def __init__(self, widget, label, prop, to, timestep, length,
+                 func=AnimationAlpha.ramp):
+        self.widget     = widget
+        self.frame      = 0.0
+        self.prop       = prop
+        self.value_to   = to
+        self.value_from = self.get_value_from()
+        self.timestep   = timestep
+        self.length     = length
+        self.label      = label
+        self.func       = func
+
+    def get_current_value(self):
+        return self.func(self.value_from, self.value_to,
+                         self.length, self.frame)
+
+    def start(self):
+        self.reset()
+        pyglet.clock.schedule_once(self.advance_frame, 1/60.0)
+        self.widget.dispatch_event('on_animation_start', self)
+
+    def reset(self):
+        self.value_from = self.get_value_from()
+        self.frame = 0.0
+        self.widget.dispatch_event('on_animation_reset', self)
+
+    def advance_frame(self, dt):
+        self.frame += self.timestep
+        self.set_value_from(self.get_current_value())
+        if self.frame < self.length:
+            pyglet.clock.schedule_once(self.advance_frame, 1/60.0)
+        else:
+            self.widget.dispatch_event('on_animation_complete', self)
+
+    def get_value_from(self):
+        if hasattr(self.widget, self.prop):
+            return self.widget.__getattribute__(self.prop)
+        return self.widget.__dict__[self.prop]
+
+    def set_value_from(self, value):
+        if hasattr(self.widget, self.prop):
+            self.widget.__setattr__(self.prop, value)
+        else:
+            self.widget.__dict__[self.prop] = value
+
+
 class MTWidgetFactory(object):
     _widgets = {}
 
@@ -113,8 +179,9 @@ class MTWidget(pyglet.event.EventDispatcher):
         except:
             pass
 
-    def add_animation(self, label, prop, to , timestep, length):
-        anim = Animation(self, label, prop, to, timestep, length)
+    def add_animation(self, label, prop, to , timestep, length,
+                      func=AnimationAlpha.ramp):
+        anim = Animation(self, label, prop, to, timestep, length, func=func)
         self.animations.append(anim)
         return anim
 
@@ -222,70 +289,6 @@ class MTWindow(TouchWindow):
     def on_object_up(self, touches, touchID, id, x, y,angle):
         self.root.dispatch_event('on_object_up', touches, touchID, id, x, y,angle)
 
-
-class AnimationAlpha(object):
-    """Collection of animation function, to be used with Animation object."""
-    @staticmethod
-    def ramp(value_from, value_to, length, frame):
-        return (1.0 - frame / length) * value_from  +  frame / length * value_to
-
-    @staticmethod
-    def sin(value_from, value_to, length, frame):
-        return math.sin(math.pi / 2 * (1.0 - frame / length)) * value_from  + \
-            math.sin(math.pi / 2  * (frame / length)) * value_to
-
-    @staticmethod
-    def bubble(value_from, value_to, length, frame):
-        return math.sin(math.pi * (1.0 - frame / length)) * value_from  + \
-            math.sin(math.pi  * (frame / length)) * value_to
-
-
-class Animation(object):
-    """Class to animate property over the time"""
-    def __init__(self, widget, label, prop, to, timestep, length,
-                 func=AnimationAlpha.ramp):
-        self.widget     = widget
-        self.frame      = 0.0
-        self.prop       = prop
-        self.value_to   = to
-        self.value_from = self.get_value_from()
-        self.timestep   = timestep
-        self.length     = length
-        self.label      = label
-        self.func       = func
-
-    def get_current_value(self):
-        return self.func(self.value_from, self.value_to,
-                         self.length, self.frame)
-
-    def start(self):
-        self.reset()
-        pyglet.clock.schedule_once(self.advance_frame, 1/60.0)
-        self.widget.dispatch_event('on_animation_start', self)
-
-    def reset(self):
-        self.value_from = self.get_value_from()
-        self.frame = 0.0
-        self.widget.dispatch_event('on_animation_reset', self)
-
-    def advance_frame(self, dt):
-        self.frame += self.timestep
-        self.set_value_from(self.get_current_value())
-        if self.frame < self.length:
-            pyglet.clock.schedule_once(self.advance_frame, 1/60.0)
-        else:
-            self.widget.dispatch_event('on_animation_complete', self)
-
-    def get_value_from(self):
-        if hasattr(self.widget, self.prop):
-            return self.widget.__getattribute__(self.prop)
-        return self.widget.__dict__[self.prop]
-
-    def set_value_from(self, value):
-        if hasattr(self.widget, self.prop):
-            self.widget.__setattr__(self.prop, value)
-        else:
-            self.widget.__dict__[self.prop] = value
 
 class MTDisplay(MTWidget):
     """MTDisplay is a widget that draw a circle under every touch on window"""

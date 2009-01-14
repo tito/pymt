7df4516976554369563bea1b8c238fc66f117dd4
thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-14 19:48:30 +0000

    git-svn-id: https://pymt.googlecode.com/svn/trunk@81 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    committer: thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/pictures/animation.py
+++ examples/pictures/animation.py
@@ -1,23 +1,31 @@
 from pymt import *
-import random
 
-c = MTWidget()
+c = MTContainer()
 
-class MTScatteredObj(MTScatterImage):
-	"""MTScatteredObj is a zoomable Image widget with a possibility of providing rotation during spawning"""
-	def __init__(self, img_src,parent=None, pos=(0,0), size=(100,100), rotation=45):
-		MTScatterImage.__init__(self,img_src,parent,pos,size)
-		self.rotation = rotation
-
-for i in range (10):
+for i in range (20):
 		img_src = 'bilder/testpic'+str(i+1)+'.jpg'
-		x = random.randint(100, 1000)
-		y = random.randint(100, 700)
-		size = random.uniform(0.25, 4.1)*100
-		rot = random.uniform(0, 360)
-		b = MTScatteredObj(img_src, c, (x,y),(size,size), rot)
-		c.add_widget(b)
+		x , y =  i/5*300+150, i%5*200+100
+		b = MTImageButton(img_src, parent=c, scale=0.16, pos = (x,y))
+		b.status = 'not zoomed'
 		
+		anim = b.add_animation('zoom','x', 400, 1.0/60, .2)
+		anim = b.add_animation('zoom','y', 300, 1.0/60, .2)
+		anim = b.add_animation('zoom','scale', 1.0, 1.0/60, .2)
+		
+		anim = b.add_animation('shrink','x', x, 1.0/60, .2)
+		anim = b.add_animation('shrink','y', y, 1.0/60, .2)
+		anim = b.add_animation('shrink','scale', 0.24, 1.0/60, .2)
+		
+		def click(w, touchID, x, y):
+			if w.status == 'zoomed':
+				w.start_animations('shrink')
+				w.status = 'not_zoomed'
+			else:
+				w.start_animations('zoom')
+				w.status = 'zoomed'		
+				
+		b.push_handlers(on_click=curry(click,b)  )
+		c.add_widget(b)
 		
 w = MTWindow()
 w.add_widget(c)

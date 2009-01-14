be0d25e073c8db703ed4d11435ebab076ccf30ca
thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-14 20:15:12 +0000

    changed 3dmt.py to premultiply the rotation matrix so that it always roatates the same way no matter what side of the model were looking at
    git-svn-id: https://pymt.googlecode.com/svn/trunk@82 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/3Dviewer/3dmt.py
+++ examples/3Dviewer/3dmt.py
@@ -6,49 +6,65 @@ import pickle
 
 
 class GLWindow(MTRectangularWidget):
-    def __init__(self,parent=None, size=(640,480), pos=(0,0)):
-        MTRectangularWidget.__init__(self, parent=parent,size=(640,480), pos=(0,0))
-        self.model = bunny = OBJ('monkey.obj')
-        self.draw_color = (1.0,1.0,1.0)
-        self.touch_position = {}
-        self.rot_x, self.rot_y = 0.0, 0.0
+	def __init__(self,parent=None, size=(640,480), pos=(0,0)):
+		MTRectangularWidget.__init__(self, parent=parent,size=(640,480), pos=(0,0))
+		self.model = bunny = OBJ('monkey.obj')
+		self.draw_color = (1.0,1.0,1.0)
+		self.touch_position = {}
+		self.rot_x, self.rot_y = 0.0, 0.0
+		self.rotation_matrix = (GLfloat * 16)()
+		glMatrixMode(GL_MODELVIEW)
+		glLoadIdentity()
+		glGetFloatv(GL_MODELVIEW_MATRIX, self.rotation_matrix)
 
-    def draw(self):
-        glEnable(GL_LIGHTING)
-        glEnable(GL_LIGHT0)
-        glEnable(GL_NORMALIZE)
-        glEnable(GL_DEPTH_TEST)
 
-        glMatrixMode(GL_PROJECTION)
-        glPushMatrix()
-        glLoadIdentity()
-        gluPerspective(60.,self.width/float(self.height) , 1., 100.)
+	def on_draw(self):
+		glEnable(GL_LIGHTING)
+		glEnable(GL_LIGHT0)
+		glEnable(GL_NORMALIZE)
+		glEnable(GL_DEPTH_TEST)
+		
+		glMatrixMode(GL_PROJECTION)
+		glPushMatrix()
+		glLoadIdentity()
+		gluPerspective(60.,self.width/float(self.height) , 1., 100.)
+		
+		glMatrixMode(GL_MODELVIEW)
+		glPushMatrix()
+		glLoadIdentity()
+		glTranslatef(0.0,0.0,-3.0)
+		
+		self.draw()
+		
+		glPopMatrix()
+		glMatrixMode(GL_PROJECTION)
+		glPopMatrix()
 
-        glMatrixMode(GL_MODELVIEW)
-        glPushMatrix()
-        glLoadIdentity()
-        glTranslatef(0.0,0.0,-3.0)
+	def draw(self):
+		glMultMatrixf(self.rotation_matrix)
+		glRotatef(180.0, 0,0,1)
+		glRotatef(90.0, 1,0,0)
+		self.model.draw()
 
-        glRotatef(-self.rot_x, 1, 0, 0)
-        glRotatef(self.rot_y, 0, 1, 0)
-        glRotatef(180.0, 0,0,1)
-        glRotatef(90.0, 1,0,0)
-        self.model.draw()
 
-        glPopMatrix()
-        glMatrixMode(GL_PROJECTION)
-        glPopMatrix()
 
-    def on_touch_down(self, touches, touchID, x, y):
-        self.touch_position[touchID] = (x,y)
-
-    def on_touch_move(self, touches, touchID, x, y):
-        self.rot_x += 200.0*(y-self.touch_position[touchID][1])/self.width
-        self.rot_y += 200.0*(x-self.touch_position[touchID][0])/self.height
-        self.touch_position[touchID] = (x,y)
-
-    def on_touch_up(self, touches, touchID, x, y):
-        del self.touch_position[touchID]
+	def on_touch_down(self, touches, touchID, x, y):
+		self.touch_position[touchID] = (x,y)
+	
+	def on_touch_move(self, touches, touchID, x, y):
+		dx = 100.0*(x-self.touch_position[touchID][0])/float(self.width)
+		dy = 100.0*(y-self.touch_position[touchID][1])/float(self.height)		
+		glMatrixMode(GL_MODELVIEW)
+		glLoadIdentity()
+		glRotatef(dx, 0,1,0)
+		glRotatef(-dy, 1,0,0)
+		glMultMatrixf(self.rotation_matrix)
+		glGetFloatv(GL_MODELVIEW_MATRIX, self.rotation_matrix)
+	
+		self.touch_position[touchID] = (x,y)
+	
+	def on_touch_up(self, touches, touchID, x, y):
+		del self.touch_position[touchID]
 
 w = MTWindow()
 w.set_fullscreen()

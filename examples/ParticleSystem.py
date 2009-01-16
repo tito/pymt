c3b028e2ae1572dcb13af95f40e54dffa1112eac
sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-16 15:37:00 +0000

    Finally the efficiently working ParticleEngine code, Txprog optimized it, now its performing the best :)
    git-svn-id: https://pymt.googlecode.com/svn/trunk@98 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/ParticleSystem.py
+++ examples/ParticleSystem.py
@@ -1,84 +1,108 @@
 from pymt import *
 import random
 
-class particle(MTRectangularWidget):
-    def __init__(self, parent, pos=(0, 0), size=(100, 100), color=(0, 0, 0, 1.0),rotation=45, **kargs):
-        MTRectangularWidget.__init__(self,parent, pos, size, **kargs)
+class ParticleObject(MTWidget):
+    def __init__(self, parent=None, pos=(0,0), size=(15,15), color=(1,1,1),
+                 rotation=45, **kargs):
+        MTWidget.__init__(self, parent)
         self.x, self.y = pos
-        self.width, self.height = size
-        self.red = color[0]
-        self.green = color[1]
-        self.blue = color[2]		
-        self.opacity = color[3]
-        self.rotation = rotation
-        self.zoom = 1.0
+        self.size = size
+        self.color = color
+        self.rotation = 0
+        self.opacity = 1
+        self.zoom = 1
+
+    def animate(self):
+        self.show()
+        self.from_x = self.x
+        self.from_y = self.y
+        self.to_x = int(random.uniform(self.x-100, self.x+100))
+        self.to_y = int(random.uniform(self.y-100, self.y+100))
+        self.length = random.uniform(0.2, 0.25)
+        self.timestep = 1.0/60
+        self.frame = 0
+
+    def advance_frame(self, dt):
+        self.frame += self.timestep
+        self.x = AnimationAlpha.ramp(self.from_x, self.to_x, self.length, self.frame)
+        self.y = AnimationAlpha.ramp(self.from_y, self.to_y, self.length, self.frame)
+        self.rotation = AnimationAlpha.ramp(0, 360, self.length, self.frame)
+        self.zoom = AnimationAlpha.ramp(1, 0, self.length, self.frame)
+        self.opacity = AnimationAlpha.ramp(1, 0, self.length, self.frame)
+        if self.frame >= self.length:
+            self.hide()
 
     def draw(self):
-        enable_blending()	
-        glColor4d(self.red,self.green,self.blue,self.opacity)
-        drawRectangle((self.x, self.y), (self.width, self.height))
-		
+        if not self.visible:
+            return
+        glColor4f(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1), self.opacity)
+        drawRectangle((self.x, self.y), self.size)
+
     def on_draw(self):
         glPushMatrix()
         glTranslatef(self.x, self.y, 0)
         glRotated(self.rotation, 0,0,1)
         glScalef(self.zoom, self.zoom, 1)
         glTranslatef(-self.x, -self.y, 0)
-        glTranslatef(-self.width/2, -self.height/2, 0)
+        glTranslatef(-self.size[0]/2, -self.size[1]/2, 0)
         self.draw()
         glPopMatrix()
-		
+
     def on_animation_complete(self, anim):
-        self.parent.remove_widget(self)
-        
+        self.hide()
 
+class ParticleEngine(MTWidget):
+    def __init__(self, parent=None, max=100, **kargs):
+        MTWidget.__init__(self,parent,**kargs)
 
-class particleEngine(MTWidget):
-    def __init__(self,parent,numParticles=0, origin=(500,400),**kargs):
-        MTWidget.__init__(self,parent,**kargs)	
-        print "Particle Engine Initialized"
-        self.num_of_particles = numParticles
-        self.originX = origin[0] 
-        self.originY = origin[1]
-		
-    def generateParticles(self):
-        for i in range(self.num_of_particles):
-            #p = particle(None,(self.originX,self.originY),(25,25),(random.uniform(0, 1),random.uniform(0, 1),random.uniform(0, 1),1))
-            p = particle(None,(self.originX,self.originY),(5,5),(1,1,1,1))
-            xDir = int(random.uniform(self.originX-100, self.originX+100))
-            yDir = int(random.uniform(self.originY-100, self.originY+100))
-            plife = random.uniform(0.2, 0.25)
-            anim = p.add_animation('rotScaleFade','x', xDir, 1.0/60, plife)
-            anim = p.add_animation('rotScaleFade','y', yDir, 1.0/60, plife)
-            anim = p.add_animation('rotScaleFade','rotation',360, 1.0/60, plife)
-            anim = p.add_animation('rotScaleFade','zoom',0.0, 1.0/60, plife)
-            anim = p.add_animation('rotScaleFade','opacity',0, 1.0/60, plife)		
-            self.add_widget(p)		
-            p.start_animations('rotScaleFade')
-			
-			
+        print 'Particle Engine Initialized'
+        self.max        = max
+        self.particles  = {}
+        for i in range(self.max):
+            self.particles[i] = ParticleObject()
+            self.particles[i].hide()
+            self.add_widget(self.particles[i])
+
+        pyglet.clock.schedule_once(self.advance_frame, 1/60.0)
+
+    def draw(self):
+        enable_blending()
+        MTWidget.draw(self)
+
+    def generate(self, pos, count):
+        for i in range(self.max):
+            if self.particles[i].visible:
+                continue
+            count = count - 1
+            if count <= 0:
+                return
+            self.particles[i].x, self.particles[i].y = pos
+            self.particles[i].animate()
+
+    def advance_frame(self, dt):
+        for i in range(self.max):
+            if self.particles[i].visible:
+                self.particles[i].advance_frame(dt)
+        pyglet.clock.schedule_once(self.advance_frame, 1/60.0)
+
 class ParticleShow(MTWindow):
-	def on_touch_down(self, touches, touchID, x,y):
-			print "Background Touched"
-			pe = particleEngine(self,5,(x,y))
-			w.add_widget(pe)
-			pe.generateParticles()
-			return True
-	
-	def on_touch_move(self, touches, touchID, x,y):
-			print "Background Touched"
-			pe = particleEngine(self,5,(x,y))
-			w.add_widget(pe)
-			pe.generateParticles()
-			return True
-			
+    def __init__(self):
+        MTWindow.__init__(self)
+        self.pe = ParticleEngine()
+        self.add_widget(self.pe)
+
+    def on_touch_down(self, touches, touchID, x,y):
+        print 'Background Touched'
+        self.pe.generate((x, y), 5)
+        return True
+
+    def on_touch_move(self, touches, touchID, x,y):
+        print 'Background Touched'
+        self.pe.generate((x, y), 5)
+        return True
+
 #start the application (inits and shows all windows)
 if __name__ == '__main__':
-	w = ParticleShow()
-	#w.set_fullscreen()
-	pe = particleEngine(None,50)
-	w.add_widget(pe)
-	pe.generateParticles()
-	
-
-	runTouchApp()
+    w = ParticleShow()
+    w.set_fullscreen()
+    runTouchApp()

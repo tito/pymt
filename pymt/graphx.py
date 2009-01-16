c6bf21df6e1e1a91855d04b911a158219459f6ba
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-16 16:32:41 +0000

    add drawRoundedRectangle() for drawing rounded rectangle
    git-svn-id: https://pymt.googlecode.com/svn/trunk@99 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/graphx.py
+++ pymt/graphx.py
@@ -65,6 +65,53 @@ def paintLine(points):
     glDisable(_brush_texture.target)
 
 
+def drawRoundedRectangle(pos=(0,0), size=(100,50), radius=5, color=(1,1,1,1),
+                         linewidth=1.5, precision=0.5):
+    x, y = pos
+    w, h = size
+
+    glColor4f(*color)
+    glLineWidth(linewidth)
+
+    glBegin(GL_POLYGON)
+
+    glVertex2f(x + radius, y)
+    glVertex2f(x + w-radius, y)
+    t = math.pi * 1.5
+    while t < math.pi * 2:
+        sx = x + w - radius + math.cos(t) * radius
+        sy = y + radius + math.sin(t) * radius
+        glVertex2f(sx, sy)
+        t += precision
+
+    glVertex2f(x + w, y + radius)
+    glVertex2f(x + w, y + h - radius)
+    t = 0
+    while t < math.pi * 0.5:
+        sx = x + w - radius + math.cos(t) * radius
+        sy = y + h -radius + math.sin(t) * radius
+        glVertex2f(sx, sy)
+        t += precision
+
+    glVertex2f(x + w -radius, y + h)
+    glVertex2f(x + radius, y + h)
+    t = math.pi * 0.5
+    while t < math.pi:
+        sx = x  + radius + math.cos(t) * radius
+        sy = y + h - radius + math.sin(t) * radius
+        glVertex2f(sx, sy)
+        t += precision
+
+    glVertex2f(x, y + h - radius)
+    glVertex2f(x, y + radius)
+    t = math.pi
+    while t < math.pi * 1.5:
+        sx = x + radius + math.cos(t) * radius
+        sy = y + radius + math.sin(t) * radius
+        glVertex2f (sx, sy)
+        t += precision
+
+    glEnd()
 
 def drawCircle(pos=(0,0), radius=1.0):
     x, y = pos[0], pos[1]
@@ -103,25 +150,25 @@ class Fbo:
 		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, self.framebuffer)
 		glPushAttrib(GL_VIEWPORT_BIT)
 		glViewport(0,0,self.size[0], self.size[1])
-	
+
 	def release(self):
 		glPopAttrib()
 		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0)
-	
+
 	def __init__(self, size=(1024,1024)):
 		self.framebuffer = c_uint(0)
 		self.depthbuffer = c_uint(0)
 		self.texture = c_uint(0)
 		self.size=size
-		
+
 		glGenFramebuffersEXT(1,byref(self.framebuffer))
 		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, self.framebuffer)
-		
+
 		glGenRenderbuffersEXT(1, byref(self.depthbuffer));
 		glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, self.depthbuffer)
 		glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT, size[0], size[1])
 		glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, self.depthbuffer)
-		
+
 		glGenTextures (1, byref(self.texture))
 		glBindTexture (GL_TEXTURE_2D, self.texture)
 		glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
@@ -130,13 +177,13 @@ class Fbo:
 		glFramebufferTexture2DEXT (GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, self.texture, 0)
 		glBindRenderbufferEXT (GL_RENDERBUFFER_EXT, 0)
 		glBindFramebufferEXT (GL_FRAMEBUFFER_EXT, 0)
-								
+
 		status = glCheckFramebufferStatusEXT (GL_FRAMEBUFFER_EXT);
 		if status != GL_FRAMEBUFFER_COMPLETE_EXT:
 			print "Error in framebuffer activation"
-	
+
 	def __del__(self):
 		glDeleteFramebuffersEXT(1, byref(self.framebuffer))
 		glDeleteRenderbuffersEXT(1, byref(self.depthbuffer))
-		
+
 

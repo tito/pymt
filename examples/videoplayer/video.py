087df2e336977fe496829b1aa7b3790320a37ddd
sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-19 17:43:40 +0000

    Working properly, need to add buttons for playback control and seeking
    git-svn-id: https://pymt.googlecode.com/svn/trunk@123 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/videoplayer/video.py
+++ examples/videoplayer/video.py
@@ -5,7 +5,7 @@ from pyglet.media import *
 
 class MTVideoObj(MTScatterWidget):
     """MTScatteredObj is a zoomable Image widget with a possibility of providing rotation during spawning"""
-    def __init__(self, parent=None, pos=(0,0), size=(0,0), rotation=0):
+    def __init__(self, parent=None, pos=(300,400), size=(0,0), rotation=0):
         MTScatterWidget.__init__(self,parent,pos,size)
         self.rotation = rotation
         self.x = pos[0]
@@ -13,46 +13,25 @@ class MTVideoObj(MTScatterWidget):
         self.player = Player()
         self.source = pyglet.media.load('video.avi')
         self.player.queue(self.source)
+        self.player.eos_action = "pause"		
         self.width = self.player.get_texture().width
         self.height = self.player.get_texture().height
+        self.texW = self.player.get_texture().width
+        self.texH = self.player.get_texture().height
   
     def play(self):
         self.player.play()
-
+		
     def draw(self):
         glPushMatrix()
+        glColor3d(1,1,1)		
         glTranslated(self.x, self.y, 0)
-        glRotated(self.rotation, 0,0,1)
-        glColor3d(1,1,1)
-        glScalef(self.zoom, self.zoom, 1)		
-        self.player.get_texture().blit(0,0)
-        glPopMatrix()	
-
-		
-    """def on_draw(self):
-
-        glScalef(self.zoom, self.zoom, 1)		
-        self.draw()
-        glPopMatrix()		
-	
-        #self.image  = self.player.get_texture().blit(0,0)
-        glPushMatrix()
-        glTranslated(self.x, self.y, 0)
-        #glScaled(float(self.width)/self.source.width, float(self.height)/self.source.height, 2.0)
-        self.player.get_texture().blit(self.x,self.y)#self.image.draw()
+        glScaled(float(self.width)/self.texW, float(self.height)/self.texH, 2.0)
+        try:
+            self.player.get_texture().blit(0,0)
+        except:
+            pass
         glPopMatrix()
-		
-   def on_draw(self):
-        glPushMatrix()
-        glTranslatef(self.x, self.y, 0)
-
-        glScalef(self.zoom, self.zoom, 1)
-        glTranslatef(-self.x, -self.y, 0)
-        glTranslatef(-self.width/2, -self.height/2, 0)
-        #glColor3d(0,0,0)
-        self.draw()
-        glPopMatrix()		
-"""
 
 if __name__ == '__main__':
     w = MTWindow()

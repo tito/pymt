b3e2c1528e1b1880223a90bc997659354ec2efc4
thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-19 18:40:18 +0000

    fixed back since I accidently overwrote the change  :P
    git-svn-id: https://pymt.googlecode.com/svn/trunk@126 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/videoplayer/video.py
+++ examples/videoplayer/video.py
@@ -5,7 +5,7 @@ from pyglet.media import *
 
 class MTVideoObj(MTScatterWidget):
     """MTScatteredObj is a zoomable Image widget with a possibility of providing rotation during spawning"""
-    def __init__(self, parent=None, pos=(300,400), size=(0,0), rotation=0):
+    def __init__(self, parent=None, pos=(0,0), size=(0,0), rotation=0):
         MTScatterWidget.__init__(self,parent,pos,size)
         self.rotation = rotation
         self.x = pos[0]
@@ -13,25 +13,20 @@ class MTVideoObj(MTScatterWidget):
         self.player = Player()
         self.source = pyglet.media.load('video.avi')
         self.player.queue(self.source)
-        self.player.eos_action = "pause"		
         self.width = self.player.get_texture().width
         self.height = self.player.get_texture().height
-        self.texW = self.player.get_texture().width
-        self.texH = self.player.get_texture().height
   
     def play(self):
         self.player.play()
-		
+
     def draw(self):
         glPushMatrix()
-        glColor3d(1,1,1)		
-        glTranslated(self.x, self.y, 0)
-        glScaled(float(self.width)/self.texW, float(self.height)/self.texH, 2.0)
-        try:
-            self.player.get_texture().blit(0,0)
-        except:
-            pass
-        glPopMatrix()
+        glColor3d(1,1,1)
+        self.player.get_texture().blit(0,0)
+        glPopMatrix()   
+
+                
+
 
 if __name__ == '__main__':
     w = MTWindow()

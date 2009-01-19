ad68062872183e09b099352774c2fb4247e88221
sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-19 15:56:31 +0000

    Changed from ScatterImage to ScatterWidget
    git-svn-id: https://pymt.googlecode.com/svn/trunk@121 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/videoplayer/video.py
+++ examples/videoplayer/video.py
@@ -3,10 +3,10 @@ import os
 import random
 from pyglet.media import *
 
-class MTVideoObj(MTScatterImage):
+class MTVideoObj(MTScatterWidget):
     """MTScatteredObj is a zoomable Image widget with a possibility of providing rotation during spawning"""
-    def __init__(self, img_src="../pictures/drawing.jpg",parent=None, pos=(0,0), size=(0,0), rotation=0):
-        MTScatterImage.__init__(self,img_src,parent,pos,size)
+    def __init__(self, parent=None, pos=(0,0), size=(0,0), rotation=0):
+        MTScatterWidget.__init__(self,parent,pos,size)
         self.rotation = rotation
         self.x = pos[0]
         self.y = pos[1]

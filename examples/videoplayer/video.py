4ff19d6208c0a1e229d194e78390bf4b5fe036d6
sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-19 19:13:32 +0000

    Added border :P
    git-svn-id: https://pymt.googlecode.com/svn/trunk@127 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: sharath.patali <sharath.patali@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/videoplayer/video.py
+++ examples/videoplayer/video.py
@@ -5,7 +5,7 @@ from pyglet.media import *
 
 class MTVideoObj(MTScatterWidget):
     """MTScatteredObj is a zoomable Image widget with a possibility of providing rotation during spawning"""
-    def __init__(self, parent=None, pos=(0,0), size=(0,0), rotation=0):
+    def __init__(self, parent=None, pos=(300,200), size=(0,0), rotation=0):
         MTScatterWidget.__init__(self,parent,pos,size)
         self.rotation = rotation
         self.x = pos[0]
@@ -13,14 +13,19 @@ class MTVideoObj(MTScatterWidget):
         self.player = Player()
         self.source = pyglet.media.load('video.avi')
         self.player.queue(self.source)
+        self.player.eos_action = "pause" 
         self.width = self.player.get_texture().width
         self.height = self.player.get_texture().height
+        self.texW = self.player.get_texture().width
+        self.texH = self.player.get_texture().height
   
     def play(self):
         self.player.play()
 
-    def draw(self):
+    def draw(self):        
         glPushMatrix()
+        glColor3d(0,0,0)
+        drawRectangle((-10,-10),(self.texW+20,self.texH+20))
         glColor3d(1,1,1)
         self.player.get_texture().blit(0,0)
         glPopMatrix()   

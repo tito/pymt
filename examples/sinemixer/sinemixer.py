db51e7bf7c26b43ecb50b2496b9b0465edb366ae
lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-22 19:47:56 +0000

    Functional sineplayer.py, make sure you read the comments at the top, will comment the code soon.
    git-svn-id: https://pymt.googlecode.com/svn/trunk@197 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/sinemixer/sinemixer.py
+++ examples/sinemixer/sinemixer.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+
+# Note: To use this example, you will need Csound and ounk.
+# Csound download: http://sourceforge.net/project/showfiles.php?group_id=81968
+# Ounk download: http://code.google.com/p/ounk/source/checkout (once you checkout, run setup.py)
+
 # PYMT Plugin integration
 IS_PYMT_PLUGIN = True
 PLUGIN_TITLE = 'Sine Player'
@@ -7,16 +12,22 @@ PLUGIN_DESCRIPTION = 'This plugin is a demonstration of the integration between 
 
 from pymt import *
 from pyglet.gl import *
+from ounk import ounklib as ounk
 
 def pymt_plugin_activate(w, ctx):
+    ounk.setGlobalDuration(-1)
+    ounk.oscReceive(bus=['amp0','amp1','amp2','amp3','pitch0','pitch1','pitch2','pitch3'], adress=['/amp0','/amp1','/amp2','/amp3','/pitch0','/pitch1','/pitch2','/pitch3'], port = 9005, portamento = 0.05)
+    ounk.sine(pitch=[1,1,1,1], pitchVar=['pitch0','pitch1','pitch2','pitch3'], amplitudeVar=['amp0','amp1','amp2','amp3'])
+    ounk.startCsound()
+    
     ctx.c = MTWidget()
     window_size = w.get_size()
     size = (50,350)
     sliders = []
     buttons = []
     for widget in range(4):
-        sliders.append(MTSlider(pos = (window_size[0]*((widget + 1)/5.) - size[0]/2., 90), size = size))
-        buttons.append(MTToggleButton(label = 'Hello', pos = (window_size[0]*((widget + 1)/5.) - size[0]/2., 30), size = (size[0], size[0])))
+        sliders.append(MTSlider(min = 100, max = 1000, pos = (window_size[0]*((widget + 1)/5.) - size[0]/2., 90), size = size))
+        buttons.append(MTButton(label = '', pos = (window_size[0]*((widget + 1)/5.) - size[0]/2., 30), size = (size[0], size[0])))
         ctx.c.add_widget(sliders[widget])
         ctx.c.add_widget(buttons[widget])
     s1,s2,s3,s4 = sliders
@@ -24,27 +35,30 @@ def pymt_plugin_activate(w, ctx):
     
     @s1.event
     def on_value_change(value):
-        #print('Slider1',value)
-        print(b1.get_state())
+        ounk.sendOscControl(value, adress='/pitch0', port=9005)
+        print('Slider1',value)
 
     @s2.event
     def on_value_change(value):
-        #print('Slider2',value)
-        b1.set_state('normal')
+        ounk.sendOscControl(value, adress='/pitch1', port=9005)
+        print('Slider2',value)
     
     @s3.event
     def on_value_change(value):
-        #print('Slider3',value)
-        b1.set_state('down')
+        ounk.sendOscControl(value, adress='/pitch2', port=9005)
+        print('Slider3',value)
     
     @s4.event
     def on_value_change(value):
+        ounk.sendOscControl(value, adress='/pitch3', port=9005)
         print('Slider4',value)
    
     def on_release_callback(btn, touchID, x,y):
+        ounk.sendOscControl(0, adress='/amp{0}'.format(btn), port=9005)
         print('Button {0} released'.format(btn+1))
         
     def on_press_callback(btn, touchID, x, y):
+        ounk.sendOscControl(1, adress='/amp{0}'.format(btn), port=9005)
         print('Button {0} pressed'.format(btn+1))
     
     for b in range(4):
@@ -55,6 +69,7 @@ def pymt_plugin_activate(w, ctx):
 
 def pymt_plugin_deactivate(w, ctx):
     w.remove_widget(ctx.c)
+    ounk.stopCsound()
 
 if __name__ == '__main__':
     w = MTWindow()

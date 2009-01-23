e6269a28e2a7da25d49cf09999cc8563c0d6480f
lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-23 04:43:37 +0000

    Commented sinemixer.py, cleaned the code.
    git-svn-id: https://pymt.googlecode.com/svn/trunk@204 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/sinemixer/sinemixer.py
+++ examples/sinemixer/sinemixer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Note: To use this example, you will need Csound and ounk.
-# Csound download: http://sourceforge.net/project/showfiles.php?group_id=81968
+# Csound download: http://sourceforge.net/project/showfiles.php?group_id=81968 (take the one labeled 'f' and don't install python support if asked)
 # Ounk download: http://code.google.com/p/ounk/source/checkout (once you checkout, run setup.py)
 
 # PYMT Plugin integration
@@ -16,11 +16,15 @@ from ounk import ounklib as ounk
 import time
 
 def pymt_plugin_activate(w, ctx):
+    
+    # A global duration of -1 will make the sound last forever
     ounk.setGlobalDuration(-1)
+    # We create buses that we will use to send control data
     ounk.oscReceive(bus=['amp0','amp1','amp2','amp3','pitch0','pitch1','pitch2','pitch3'], adress=['/amp0','/amp1','/amp2','/amp3','/pitch0','/pitch1','/pitch2','/pitch3'], port = 9005, portamento = 0.05)
+    # We create 4 sines and associate pitch variation and amplitude variation with the corresponding buses.
     ounk.sine(pitch=[1,1,1,1], pitchVar=['pitch0','pitch1','pitch2','pitch3'], amplitudeVar=['amp0','amp1','amp2','amp3'])
+    # Csound is started here
     ounk.startCsound()
-    time.sleep(1.5)
     
     ctx.c = MTWidget()
     window_size = w.get_size()
@@ -29,45 +33,35 @@ def pymt_plugin_activate(w, ctx):
     buttons = []
     for widget in range(4):
         sliders.append(MTSlider(min = 100, max = 1000, pos = (window_size[0]*((widget + 1)/5.) - size[0]/2., 90), size = size))
-        sliders[widget].set_value(300)
         buttons.append(MTButton(label = '', pos = (window_size[0]*((widget + 1)/5.) - size[0]/2., 30), size = (size[0], size[0])))
         ctx.c.add_widget(sliders[widget])
         ctx.c.add_widget(buttons[widget])
     s1,s2,s3,s4 = sliders
     b1,b2,b3,b4 = buttons
     
-    # Force init of the pitches
-    for val in range(4):
-        ounk.sendOscControl(300, adress='/pitch{0}'.format(val), port=9005)
-    
-    @s1.event
-    def on_value_change(value):
-        ounk.sendOscControl(value, adress='/pitch0', port=9005)
-        print('Slider1',value)
 
-    @s2.event
-    def on_value_change(value):
-        ounk.sendOscControl(value, adress='/pitch1', port=9005)
-        print('Slider2',value)
+    # This function gets called when a slider moves, it sets the pitch of each sine.
+    def on_value_change_callback(slider, value):
+        ounk.sendOscControl(value, adress='/pitch{0}'.format(slider), port=9005)
     
-    @s3.event
-    def on_value_change(value):
-        ounk.sendOscControl(value, adress='/pitch2', port=9005)
-        print('Slider3',value)
-    
-    @s4.event
-    def on_value_change(value):
-        ounk.sendOscControl(value, adress='/pitch3', port=9005)
-        print('Slider4',value)
-   
-    def on_release_callback(btn, touchID, x,y):
-        ounk.sendOscControl(0, adress='/amp{0}'.format(btn), port=9005)
-        print('Button {0} released'.format(btn+1))
-        
+    # We push the handlers and feed it with the slider number so the callback function knows which sine to work on.
+    # The time.sleep seems necessary to let Csound the time to initialize itself.
+    for s in range(4):
+        sliders[s].push_handlers(on_value_change = curry(on_value_change_callback, s))
+        time.sleep(2)
+        sliders[s].set_value(300)
+  
+    # When the button is pressed, the amplitude is changed to 0.5
     def on_press_callback(btn, touchID, x, y):
         ounk.sendOscControl(0.5, adress='/amp{0}'.format(btn), port=9005)
-        print('Button {0} pressed'.format(btn+1))
+        #print('Button {0} pressed'.format(btn+1))
     
+    # When the button is released, the amplitude goes back to 0
+    def on_release_callback(btn, touchID, x,y):
+        ounk.sendOscControl(0, adress='/amp{0}'.format(btn), port=9005)
+        #print('Button {0} released'.format(btn+1))
+    
+    # Handlers for the buttons are pushed here.
     for b in range(4):
         buttons[b].push_handlers(on_press = curry(on_press_callback, b))
         buttons[b].push_handlers(on_release = curry(on_release_callback, b))
@@ -76,6 +70,7 @@ def pymt_plugin_activate(w, ctx):
 
 def pymt_plugin_deactivate(w, ctx):
     w.remove_widget(ctx.c)
+    # Csound needs to be stopped at the end to avoid errors.
     ounk.stopCsound()
 
 if __name__ == '__main__':

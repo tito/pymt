8528839270153fb032178a02e2d05a28210d2881
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-01-29 22:06:08 +0000

    MTWindowWallpaper: implement repeat wallpaper
    git-svn-id: https://pymt.googlecode.com/svn/trunk@277 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/ui/wallpaperwindow.py
+++ pymt/ui/wallpaperwindow.py
@@ -5,20 +5,37 @@ class MTWallpaperWindow(MTWindow):
 
     NOREPEAT = 0
     CENTER = 1
+    REPEAT = 2
 
-    def __init__(self, wallpaper=None, **kargs):
-        super(MTWallpaperWindow, self).__init__(**kargs)
-        self.wallpaper = wallpaper
-        self.position = MTWallpaperWindow.CENTER
+    def __init__(self, **kwargs):
+        kwargs.setdefault('wallpaper', None)
+        kwargs.setdefault('position', MTWallpaperWindow.CENTER)
+        super(MTWallpaperWindow, self).__init__(**kwargs)
+        self.wallpaper  = kwargs.get('wallpaper')
+        self.position   = kwargs.get('position')
 
     def draw(self):
         if self.position == MTWallpaperWindow.NOREPEAT:
+            super(MTWallpaperWindow, self).draw()
             self.wallpaper.x, self.wallpaper.y = (0, 0)
             self.wallpaper.draw()
         elif self.position == MTWallpaperWindow.CENTER:
+            super(MTWallpaperWindow, self).draw()
             self.wallpaper.x = (self.size[0] - self.wallpaper.width) / 2
             self.wallpaper.y = (self.size[1] - self.wallpaper.height) / 2
             self.wallpaper.draw()
+        elif self.position == MTWallpaperWindow.REPEAT:
+            r_x = float(self.size[0]) / self.wallpaper.width
+            r_y = float(self.size[1]) / self.wallpaper.height
+            if int(r_x) != r_x:
+                r_x = int(r_x) + 1
+            if int(r_y) != r_y:
+                r_y = int(r_y) + 1
+            for x in range(0, int(r_x)):
+                for y in range(0, int(r_y)):
+                    self.wallpaper.x = x * self.wallpaper.width
+                    self.wallpaper.y = y * self.wallpaper.height
+                    self.wallpaper.draw()
 
     def _set_wallpaper(self, image):
         if not image:

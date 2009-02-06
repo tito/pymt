ac25c7def12614818aa7c0355804ae618bd8111b
lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-06 06:39:48 +0000

    Small correction in svg.py
    git-svn-id: https://pymt.googlecode.com/svn/trunk@315 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/svg/svg.py
+++ examples/svg/svg.py
@@ -6,10 +6,14 @@ class MTSquirtle(MTScatterWidget):
     def __init__(self, **kwargs):
         super(MTSquirtle, self).__init__(**kwargs)
         squirtle.setup_gl()
-        self.sun = squirtle.SVG("squirtle/svgs/sun.svg", anchor_x='center', anchor_y= 'center')
+        self.sun = squirtle.SVG("squirtle/svgs/sun.svg", anchor_x= 'center', anchor_y= 'center')
+        self.height = self.sun.height
+        self.width = self.sun.width
+        self.init_transform((250,200),0)
     
-    def draw(self):
-        self.sun.draw(self.x + self.width/2, self.y + self.height/2)
+    def draw(self):      
+        glColor4d(*self.color)
+        self.sun.draw(self.pos[0] + self.width/2, self.pos[1] + self.height/2)
 
 if __name__ == '__main__':
     w = MTWindow()

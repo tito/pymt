81fd61bfe411fbcc2ed0a1b4fcdc7babbfcc2f1d
lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-06 07:04:45 +0000

    Nicer svg example
    git-svn-id: https://pymt.googlecode.com/svn/trunk@316 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/svg/svg.py
+++ examples/svg/svg.py
@@ -4,19 +4,29 @@ from pymt import *
 import squirtle.squirtle as squirtle
 class MTSquirtle(MTScatterWidget):
     def __init__(self, **kwargs):
+        kwargs.setdefault('filename', None)
+        if kwargs.get('filename') is None:
+            raise Exception('No filename given to MTSquirtle')
         super(MTSquirtle, self).__init__(**kwargs)
+        self.position = kwargs.get('position')
+        self.filename = kwargs.get('filename')
+        
         squirtle.setup_gl()
-        self.sun = squirtle.SVG("squirtle/svgs/sun.svg", anchor_x= 'center', anchor_y= 'center')
-        self.height = self.sun.height
-        self.width = self.sun.width
-        self.init_transform((250,200),0)
+        self.svg = squirtle.SVG(self.filename, anchor_x= 'center', anchor_y= 'center')
+        self.height = self.svg.height
+        self.width = self.svg.width
+        self.init_transform(self.position,0)
     
     def draw(self):      
         glColor4d(*self.color)
-        self.sun.draw(self.pos[0] + self.width/2, self.pos[1] + self.height/2)
+        self.svg.draw(self.pos[0] + self.width/2, self.pos[1] + self.height/2)
 
 if __name__ == '__main__':
     w = MTWindow()
-    mts = MTSquirtle()
-    w.add_widget(mts)
+    sun = MTSquirtle(filename = 'squirtle/svgs/sun.svg', position = (200,200))
+    cloud = MTSquirtle(filename = 'squirtle/svgs/cloud.svg', position = (50,100))
+    ship = MTSquirtle(filename = 'squirtle/svgs/ship.svg', position = (280,100))
+    w.add_widget(sun)
+    w.add_widget(cloud)
+    w.add_widget(ship)
     runTouchApp()

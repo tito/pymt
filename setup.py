f3b5450983a70d021672479ffe030ca173f37656
thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-09 09:55:05 +0000

    fixed setup.py to include package data
    git-svn-id: https://pymt.googlecode.com/svn/trunk@364 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from distutils.core import setup
 from distutils.sysconfig import get_python_lib
 import sys
-import os
 
 
 sys.argv.append('install')
 pylib = get_python_lib()
-svgs = ['pymt/data/icons/svg/' + s for s in os.listdir('pymt/data/icons/svg') if s.endswith('svg')]
-pngs = ['pymt/data/icons/' + p for p in os.listdir('pymt/data/icons') if p.endswith('png')]
-
 setup (name='pymt',
       version='0.1',
       author='Thomas Hansen',
@@ -16,5 +12,7 @@ setup (name='pymt',
       url='http://code.google.com/p/pymt/',
       license='GPL',
       packages=['pymt', 'pymt.osc', 'pymt.ui'],
-      data_files=[(pylib+'/pymt/data/icons', pngs), (pylib+'/pymt/data/icons/svg', svgs)]
+      package_dir={'pymt': 'pymt'},
+      package_data={'pymt': ['data/icons/*.png'],
+                    'pymt': ['data/icons/svg/*.svg']}
 )

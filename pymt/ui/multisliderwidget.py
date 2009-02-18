f446b6146c1fa8b8e1162bad6f653db713c3ad1d
lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-18 01:48:20 +0000

    Working multisliderwidget.py
    git-svn-id: https://pymt.googlecode.com/svn/trunk@395 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/ui/multisliderwidget.py
+++ pymt/ui/multisliderwidget.py
@@ -7,9 +7,10 @@ import math
 class MTMultiSlider(MTWidget):
     def __init__(self, **kwargs):
         super(MTMultiSlider, self).__init__(**kwargs)
-        kwargs.setdefault('sliders', 8)
+        kwargs.setdefault('sliders', 27)
         self._sliders = kwargs.get('sliders')
-        self._slider_values = [random.random() for x in range(self._sliders)]
+        #self._slider_values = [random.random() for x in range(self._sliders)]
+        self._slider_values = [0.5 for x in range(self._sliders)]
         self._spacing = 2
     
     def draw(self):
@@ -22,6 +23,18 @@ class MTMultiSlider(MTWidget):
             size_x = (float(self.width) / self._sliders) - self._spacing
             size_y = self.height * self._slider_values[slider]
             drawRectangle(pos = (pos_x, pos_y), size = (size_x, size_y))
+
+    def on_touch_down(self, touches, touchID, x, y):
+        if self.collide_point(x,y):
+            print self.return_slider(x)
+            self._slider_values[self.return_slider(x)] = (y - self.y) / float(self.height)
+            return True
+        
+    def on_touch_move(self, touches, touchID, x, y):
+        self.on_touch_down(touches, touchID, x, y)
+        
+    def return_slider(self, x):
+        return int((x - self.x) / float(self.width)  * self._sliders)
             
 if __name__ == '__main__':
     w = MTWindow(fullscreen=False)

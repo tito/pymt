9a1cb482262770acd89ea69a16930bf028c2c5c7
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-24 21:29:11 +0000

    form: try to implement gridlayout (wip)
    git-svn-id: https://pymt.googlecode.com/svn/trunk@483 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/widgets/form.py
+++ examples/widgets/form.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 from pymt import *
 
 '''
@@ -8,6 +9,84 @@ Thanks, tito.
 
 
 '''
+
+class HVMatrix(MTWidget):
+    def __init__(self, **kwargs):
+        kwargs.setdefault('matrix_size', (3,3))
+        kwargs.setdefault('spacing', 5)
+        super(HVMatrix, self).__init__(**kwargs)
+        self.matrix_size = kwargs.get('matrix_size')
+        self.spacing = kwargs.get('spacing')
+        self.dl = GlDisplayList()
+        self.children_active = []
+
+    def add_widget(self, widget, do_layout=True):
+        if len(self.children) >= self.matrix_size[0] * self.matrix_size[1]:
+            raise Exception('Too much children in HVMatrix. Increase your matrix_size!')
+        super(HVMatrix, self).add_widget(widget)
+        if do_layout:
+            self.layout()
+
+    def layout(self):
+        mx, my = self.matrix_size
+        spacing = self.spacing
+
+        cols = {}
+        rows = {}
+        for i in range(my):
+            cols[i] = 0
+        for i in range(mx):
+            rows[i] = 0
+
+        # calculate rows/cols size
+        i = 0
+        for col in range(my):
+            for row in range(mx):
+                if i >= len(self.children):
+                    break
+                c = self.children[i]
+                if c.width > cols[col]:
+                    cols[col] = c.width
+                if c.height > rows[row]:
+                    rows[row] = c.height
+                i = i + 1
+
+        x = spacing
+        y = spacing
+        i = 0
+        for col in range(my):
+            x = spacing
+            for row in range(mx):
+                if i >= len(self.children):
+                    break
+                c = self.children[i]
+                c.pos = (x, y)
+                c.size = (cols[col], rows[row])
+                i = i + 1
+                x = x + cols[col] + spacing
+            y = y + rows[row] + spacing
+
+        self.dl.clear()
+
+    def on_draw(self):
+        if not self.dl.is_compiled():
+            with self.dl:
+                for w in self.children:
+                    w.dispatch_event('on_draw')
+        self.draw()
+
+    def on_resize(self, w, h):
+        self.layout()
+
+    def draw(self):
+        self.dl.draw()
+        for w in self.children_active:
+            w.dispatch_event('on_draw')
+
+    def get_form(self):
+        if self.parent:
+            return self.parent.get_form()
+        return None
 
 
 
@@ -42,6 +121,24 @@ class MTForm(HVLayout):
         if form:
             form.layout()
 
+class MTFormLabel(MTForm):
+    def __init__(self, **kwargs):
+        kwargs.setdefault('label', '')
+        super(MTForm, self).__init__(**kwargs)
+        self.label = kwargs.get('label')
+
+    def _get_label(self):
+        return self._label
+    def _set_label(self, label):
+        self._label = label
+        self._label_obj = Label(text=label)
+        self.size = self._label_obj.content_width, self._label_obj.content_height
+    label = property(_get_label, _set_label)
+
+    def draw(self):
+        self._label_obj.x, self._label_obj.y = self.pos
+        self._label_obj.draw()
+
 
 class MTFormInput(MTForm, MTTextInput):
     def __init__(self, **kwargs):
@@ -67,20 +164,29 @@ class MTFormInput(MTForm, MTTextInput):
     def on_resize(self, w, h):
         MTForm.on_resize(self, w, h)
 
+MTWidgetFactory.register('MTForm', MTForm)
+MTWidgetFactory.register('MTFormInput', MTFormInput)
+MTWidgetFactory.register('MTFormLabel', MTFormLabel)
+MTWidgetFactory.register('HVMatrix', HVMatrix)
+
+xmldef = '''<?xml version="1.0" encoding="UTF-8"?>
+<MTScatterPlane>
+    <MTForm pos="(200,200)" padding="20">
+        <HVMatrix matrix_size="(2, 4)">
+            <MTFormLabel label="'Name'"/>
+            <MTFormInput id="'input_name'"/>
+            <MTFormLabel label="'Surname'"/>
+            <MTFormInput id="'input_surname'"/>
+            <MTFormLabel label="'Nickname'"/>
+            <MTFormInput id="'input_nickname'"/>
+            <MTFormLabel label="'Age'"/>
+            <MTFormInput id="'input_age'"/>
+        </HVMatrix>
+    </MTForm>
+</MTScatterPlane>
+'''
+
 w = MTWindow()
-p = MTScatterPlane()
-form = MTForm(pos=(200, 200), padding=20)
-textinput = MTFormInput(color=(.5,.5,.5,.5))
-form.add_widget(textinput)
-textinput = MTFormInput(color=(.5,.5,.5,.5))
-form.add_widget(textinput)
-textinput = MTFormInput(color=(.5,.5,.5,.5))
-form.add_widget(textinput)
-textinput = MTFormInput(color=(.5,.5,.5,.5))
-form.add_widget(textinput)
-p.add_widget(form)
-w.add_widget(p)
-
-print textinput.size, textinput.pos
-
+x = XMLWidget(xml=xmldef)
+w.add_widget(x)
 runTouchApp()

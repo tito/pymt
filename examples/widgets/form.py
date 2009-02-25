de623da44228e93c647a1f8c5be75bea6159f124
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-25 00:27:48 +0000

    form: now getting a simple form (wip)
    git-svn-id: https://pymt.googlecode.com/svn/trunk@486 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/widgets/form.py
+++ examples/widgets/form.py
@@ -10,38 +10,43 @@ Thanks, tito.
 
 '''
 
-class HVMatrix(MTWidget):
+class MTGridLayout(MTAbstractLayout):
     def __init__(self, **kwargs):
-        kwargs.setdefault('matrix_size', (3,3))
-        kwargs.setdefault('spacing', 5)
-        super(HVMatrix, self).__init__(**kwargs)
-        self.matrix_size = kwargs.get('matrix_size')
+        kwargs.setdefault('cols', None)
+        kwargs.setdefault('rows', None)
+        kwargs.setdefault('spacing', 10)
+        super(MTGridLayout, self).__init__(**kwargs)
+        self.cols = kwargs.get('cols')
+        self.rows = kwargs.get('rows')
         self.spacing = kwargs.get('spacing')
-        self.dl = GlDisplayList()
-        self.children_active = []
+
+    def get_max_widgets(self):
+        if self.cols and not self.rows:
+            return None
+        if self.rows and not self.cols:
+            return None
+        return self.rows * self.cols
 
     def add_widget(self, widget, do_layout=True):
-        if len(self.children) >= self.matrix_size[0] * self.matrix_size[1]:
-            raise Exception('Too much children in HVMatrix. Increase your matrix_size!')
-        super(HVMatrix, self).add_widget(widget)
-        if do_layout:
-            self.layout()
+        max = self.get_max_widgets()
+        if max and len(self.children) > max:
+            raise Exception('Too much children in MTGridLayout. Increase your matrix_size!')
+        super(MTGridLayout, self).add_widget(widget)
 
-    def layout(self):
-        mx, my = self.matrix_size
+    def do_layout(self):
         spacing = self.spacing
 
         cols = {}
         rows = {}
-        for i in range(my):
+        for i in range(self.cols):
             cols[i] = 0
-        for i in range(mx):
+        for i in range(self.rows):
             rows[i] = 0
 
         # calculate rows/cols size
         i = 0
-        for col in range(my):
-            for row in range(mx):
+        for row in range(self.rows):
+            for col in range(self.cols):
                 if i >= len(self.children):
                     break
                 c = self.children[i]
@@ -51,12 +56,11 @@ class HVMatrix(MTWidget):
                     rows[row] = c.height
                 i = i + 1
 
-        x = spacing
-        y = spacing
         i = 0
-        for col in range(my):
-            x = spacing
-            for row in range(mx):
+        y = self.y + spacing
+        for row in range(self.rows):
+            x = self.x + spacing
+            for col in range(self.cols):
                 if i >= len(self.children):
                     break
                 c = self.children[i]
@@ -66,72 +70,78 @@ class HVMatrix(MTWidget):
                 x = x + cols[col] + spacing
             y = y + rows[row] + spacing
 
-        self.dl.clear()
 
-    def on_draw(self):
-        if not self.dl.is_compiled():
-            with self.dl:
-                for w in self.children:
-                    w.dispatch_event('on_draw')
-        self.draw()
+        current_width = spacing * (len(cols) + 1)
+        for i in cols:
+            current_width += cols[i]
+        current_height = spacing * (len(rows) + 1)
+        for i in rows:
+            current_height += rows[i]
+
+        if current_height != self.content_height or current_width != self.content_width:
+            self.content_width = current_width
+            self.content_height = current_height
+            self.dispatch_event('on_content_resize', self.content_width, self.content_height)
+
+
+class MTFormWidget(MTWidget):
+    def __init__(self, **kwargs):
+        super(MTFormWidget, self).__init__(**kwargs)
 
     def on_resize(self, w, h):
-        self.layout()
-
-    def draw(self):
-        self.dl.draw()
-        for w in self.children_active:
-            w.dispatch_event('on_draw')
-
-    def get_form(self):
-        if self.parent:
-            return self.parent.get_form()
-        return None
+        layout = self.get_parent_layout()
+        if layout:
+            layout.do_layout()
+        super(MTFormWidget, self).on_resize(w, h)
 
 
-
-class MTForm(HVLayout):
+class MTForm(MTFormWidget):
     def __init__(self, **kwargs):
-        kwargs.setdefault('alignment', 'vertical')
-        kwargs.setdefault('invert_y', False)
-        kwargs.setdefault('border_width', 0)
-        kwargs.setdefault('border_color', (.1, .1, .1, .2))
+        kwargs.setdefault('color', (.25,.25,.25,.6))
+        kwargs.setdefault('layout', None)
         super(MTForm, self).__init__(**kwargs)
-        self.border_width = kwargs.get('border_width')
-        self.border_color = kwargs.get('border_color')
-        self.layout()
+        self.layout = kwargs.get('layout')
+
+    def _set_layout(self, layout):
+        if hasattr(self, '_layout') and self._layout:
+            super(MTForm, self).remove_widget(self._layout)
+        self._layout = layout
+        if self._layout:
+            super(MTForm, self).add_widget(self._layout)
+    def _get_layout(self):
+        return self._layout
+    layout = property(_get_layout, _set_layout)
+
+    def add_widget(self, widget):
+        self.layout.add_widget(widget)
 
     def draw(self):
-        set_color(*self.border_color)
-        if self.border_width > 0:
-            glLineWidth(self.border_width)
-        drawRectangle(pos=self.pos, size=self.size, style=GL_LINE_LOOP)
         set_color(*self.color)
         drawRectangle(pos=self.pos, size=self.size)
 
-    def get_form(self):
-        if self.__class__.__name__ == 'MTForm':
-            return self
-        if self.parent:
-            return self.parent.get_form()
-        return None
+    def get_parent_layout(self):
+        return self
 
-    def on_resize(self, w, h):
-        form = self.get_form()
-        if form:
-            form.layout()
+    def do_layout(self):
+        self.size = self.layout.content_width, self.layout.content_height
 
-class MTFormLabel(MTForm):
+    def on_move(self, x, y):
+        super(MTForm, self).on_move(x, y)
+        self.layout.pos = self.pos
+        self.layout.do_layout()
+
+
+class MTFormLabel(MTFormWidget):
     def __init__(self, **kwargs):
         kwargs.setdefault('label', '')
-        super(MTForm, self).__init__(**kwargs)
+        super(MTFormLabel, self).__init__(**kwargs)
         self.label = kwargs.get('label')
 
     def _get_label(self):
         return self._label
     def _set_label(self, label):
         self._label = label
-        self._label_obj = Label(text=label)
+        self._label_obj = Label(text=label, bold=True)
         self.size = self._label_obj.content_width, self._label_obj.content_height
     label = property(_get_label, _set_label)
 
@@ -140,13 +150,10 @@ class MTFormLabel(MTForm):
         self._label_obj.draw()
 
 
-class MTFormInput(MTForm, MTTextInput):
+class MTFormInput(MTTextInput):
     def __init__(self, **kwargs):
         kwargs.setdefault('font_size', 16)
-        MTForm.__init__(self, **kwargs)
-        MTTextInput.__init__(self, **kwargs)
-        #super(MTForm, self).__init__(**kwargs)
-        #super(MTTextInput, self).__init__(**kwargs)
+        super(MTFormInput, self).__init__(**kwargs)
         # get height of font-size
         self.label_obj.text = ' '
         self.height = self.label_obj.content_height
@@ -162,26 +169,28 @@ class MTFormInput(MTForm, MTTextInput):
         self.label_obj.draw()
 
     def on_resize(self, w, h):
-        MTForm.on_resize(self, w, h)
+        layout = self.get_parent_layout()
+        if layout:
+            layout.do_layout()
+        super(MTFormInput, self).on_resize(w, h)
+
 
 MTWidgetFactory.register('MTForm', MTForm)
 MTWidgetFactory.register('MTFormInput', MTFormInput)
 MTWidgetFactory.register('MTFormLabel', MTFormLabel)
-MTWidgetFactory.register('HVMatrix', HVMatrix)
+MTWidgetFactory.register('MTGridLayout', MTGridLayout)
 
 xmldef = '''<?xml version="1.0" encoding="UTF-8"?>
 <MTScatterPlane>
-    <MTForm pos="(200,200)" padding="20">
-        <HVMatrix matrix_size="(2, 4)">
-            <MTFormLabel label="'Name'"/>
-            <MTFormInput id="'input_name'"/>
-            <MTFormLabel label="'Surname'"/>
-            <MTFormInput id="'input_surname'"/>
-            <MTFormLabel label="'Nickname'"/>
-            <MTFormInput id="'input_nickname'"/>
-            <MTFormLabel label="'Age'"/>
-            <MTFormInput id="'input_age'"/>
-        </HVMatrix>
+    <MTForm pos="(200,200)" padding="20" layout="factory.get('MTGridLayout')(cols=2, rows=4)">
+        <MTFormLabel label="'Name'"/>
+        <MTFormInput id="'input_name'"/>
+        <MTFormLabel label="'Surname'"/>
+        <MTFormInput id="'input_surname'"/>
+        <MTFormLabel label="'Nickname'"/>
+        <MTFormInput id="'input_nickname'"/>
+        <MTFormLabel label="'Age'"/>
+        <MTFormInput id="'input_age'"/>
     </MTForm>
 </MTScatterPlane>
 '''

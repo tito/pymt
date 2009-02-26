f9af3b8748594401a375486c6b7ac4a83fa50363
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-26 22:51:50 +0000

    boxlayout: rewrite add layout part. now correctly support orientation + padding + invertion boxlayout: rename alignement to orientation
    git-svn-id: https://pymt.googlecode.com/svn/trunk@498 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/ui/widgets/layout.py
+++ pymt/ui/widgets/layout.py
@@ -73,12 +73,12 @@ class MTBoxLayout(MTAbstractLayout):
     '''Box layout can arrange item in horizontal or vertical orientation.
 
     :Parameters:
-        `alignment` : str, default is 'horizontal'
-            Alignment of widget inside layout, can be `horizontal` or `vertical`
         `padding` : int, default to 0
             Padding between the border and content
         `spacing` : int, default to 1
             Spacing between widgets
+        `orientation` : str, default is 'horizontal'
+            Orientation of widget inside layout, can be `horizontal` or `vertical`
         `uniform_width` : bool, default to False
             Try to have same width for all children
         `uniform_height` : bool, default to False
@@ -90,7 +90,7 @@ class MTBoxLayout(MTAbstractLayout):
     '''
 
     def __init__(self, **kwargs):
-        kwargs.setdefault('alignment', 'horizontal')
+        kwargs.setdefault('orientation', 'horizontal')
         kwargs.setdefault('spacing', 1)
         kwargs.setdefault('padding', 0)
         kwargs.setdefault('uniform_width', False)
@@ -98,14 +98,14 @@ class MTBoxLayout(MTAbstractLayout):
         kwargs.setdefault('invert_x', False)
         kwargs.setdefault('invert_y', False)
 
-        if kwargs.get('alignment') not in ['horizontal', 'vertical']:
-            raise Exception('Invalid alignment, only horizontal/vertical are supported')
+        if kwargs.get('orientation') not in ['horizontal', 'vertical']:
+            raise Exception('Invalid orientation, only horizontal/vertical are supported')
 
         super(MTBoxLayout, self).__init__(**kwargs)
 
         self.spacing        = kwargs.get('spacing')
         self.padding        = kwargs.get('padding')
-        self.alignment      = kwargs.get('alignment')
+        self.orientation    = kwargs.get('orientation')
         self.uniform_width  = kwargs.get('uniform_width')
         self.uniform_height = kwargs.get('uniform_height')
         self.invert_x       = kwargs.get('invert_x')
@@ -125,59 +125,68 @@ class MTBoxLayout(MTAbstractLayout):
         fire on_content_resize too. Uniform width/height are handled
         after on_content_resize.
         '''
-        start_x = cur_x = self.x + self.padding
-        start_y = cur_y = self.y + self.padding
+        max_width = max_height = 0
         current_width = current_height = 0
         for w in self.children:
             try:
+                if w.height > max_height:
+                    max_height = w.height
+                if w.width > max_width:
+                    max_width = w.width
+                if self.orientation == 'horizontal':
+                    if current_width > 0:
+                        current_width += self.spacing
+                    current_width += w.width
+                elif self.orientation == 'vertical':
+                    if current_height > 0:
+                        current_height += self.spacing
+                    current_height += w.height
+            except:
+                pass
+
+        # uniform
+        if self.uniform_width:
+            for w in self.children:
+                w.width = max_width
+            if self.orientation == 'horizontal':
+                current_width = (len(self.children) - 1) * (max_width + self.spacing)
+        if self.uniform_height:
+            for w in self.children:
+                w.height = max_height
+            if self.orientation == 'vertical':
+                current_height = (len(self.children) - 1) * (max_height + self.spacing)
+
+        # adjust current width/height
+        if self.orientation == 'horizontal':
+            current_height = max_height
+        elif self.orientation == 'vertical':
+            current_width = max_width
+
+        # apply double padding
+        current_width += self.padding * 2
+        current_height += self.padding * 2
+
+        # reposition
+        cur_x = self.x + self.padding
+        cur_y = self.y + self.padding
+        for w in self.children:
+            try:
                 if self.invert_x:
-                    w.x = self.width - cur_x
+                    w.y = self.x + current_width - w.width - (cur_x - self.x)
                 else:
                     w.x = cur_x
                 if self.invert_y:
-                    w.y = self.height - cur_y
+                    w.y = self.y + current_height - w.height - (cur_y - self.y)
                 else:
                     w.y = cur_y
-                if w.height > current_height:
-                    current_height = w.height
-                if w.width > current_width:
-                    current_width = w.width
-                if self.alignment == 'horizontal':
+                if self.orientation == 'horizontal':
                     cur_x += w.width + self.spacing
-                elif self.alignment == 'vertical':
+                elif self.orientation == 'vertical':
                     cur_y += w.height + self.spacing
             except:
                 pass
 
-        # apply double padding
-        cur_x = cur_x + self.padding * 2
-        cur_y = cur_y + self.padding * 2
-
-        # save
-        max_w_height = current_height
-        max_w_width  = current_width
-
-        # apply double padding
-        current_width = current_width + self.padding * 2
-        current_height = current_height + self.padding * 2
-
-        # update content size
-        new_width  = current_width
-        new_height = current_height
-        if self.alignment == 'horizontal':
-            new_width = cur_x - start_x
-        elif self.alignment == 'vertical':
-            new_height = cur_y - start_y
-
-        do_event_content_resize = (self.content_width != new_width) or (self.content_height != new_height)
-        self.content_size = (new_width, new_height)
-        if do_event_content_resize:
-            if self.uniform_width:
-                for w in self.children:
-                    w.width = max_w_width
-            if self.uniform_height:
-                for w in self.children:
-                    w.height = max_w_height
+        self.content_size = (current_width, current_height)
 
         # XXX make it optionnal, in 0.2
         self.size = (self.content_width, self.content_height)

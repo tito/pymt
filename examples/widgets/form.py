60b5513910d1c94b0388fd8b3b77222859434df9
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-02-28 10:55:21 +0000

    form: try using checkbox
    git-svn-id: https://pymt.googlecode.com/svn/trunk@518 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/widgets/form.py
+++ examples/widgets/form.py
@@ -4,22 +4,35 @@ from pymt import *
 xmldef = '''<?xml version="1.0" encoding="UTF-8"?>
 <MTScatterPlane>
     <MTForm pos="(200,200)"
-    layout="factory.get('MTGridLayout')(spacing=20, cols=2, rows=5, uniform_height=True)">
-        <MTFormLabel label="'Name'" halign="'right'"/>
-        <MTFormInput id="'input_name'"/>
-        <MTFormLabel label="'Surname'" halign="'right'"/>
-        <MTFormInput id="'input_surname'"/>
-        <MTFormLabel label="'Nickname'" halign="'right'"/>
-        <MTFormInput id="'input_nickname'"/>
-        <MTFormLabel label="'Age'" halign="'right'"/>
-        <MTFormInput id="'input_age'"/>
-        <MTFormButton label="'Send'"/>
-        <MTFormButton label="'Cancel'"/>
+    layout="factory.get('MTGridLayout')(spacing=20, cols=2, rows=7, uniform_height=True)">
+        <MTFormLabel label="'Fullscreen'" halign="'right'"/>
+        <MTFormCheckbox halign="'left'" id="'chk_fullscreen'"/>
+        <MTFormLabel label="'Show FPS'" halign="'right'"/>
+        <MTFormCheckbox halign="'left'" id="'chk_fps'"/>
+        <MTFormButton label="'Save'"/>
+        <MTFormButton label="'Close'"/>
     </MTForm>
 </MTScatterPlane>
 '''
 
 w = MTWindow()
+
+def action_fullscreen(checked):
+    global w
+    w.set_fullscreen(checked)
+
+def action_fps(checked):
+    global w
+    w.show_fps = checked
+
+
 w.add_widget(XMLWidget(xml=xmldef))
-w.add_widget(MTPopup(width=300, content="Welcome to PyMT Widget Demonstration\nEnjoy this popup !"))
+
+chk_fullscreen = getWidgetById('chk_fullscreen')
+chk_fullscreen.push_handlers(on_check=action_fullscreen)
+chk_fps = getWidgetById('chk_fullscreen')
+chk_fps.push_handlers(on_check=action_fps)
+
+
+#w.add_widget(MTPopup(width=300, content="Welcome to PyMT Widget Demonstration\nEnjoy this popup !"))
 runTouchApp()

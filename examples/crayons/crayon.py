5fc134256591ba0e0380cc439160a939821e1f46
thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-02 10:27:46 +0000

    started new crayon example. doesnt do much yet.  will hopefully turn into multi user paint app with different pens/crayons for using seperate colors
    git-svn-id: https://pymt.googlecode.com/svn/trunk@567 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: thomas.hansen <thomas.hansen@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

---
+++ examples/crayons/crayon.py
@@ -0,0 +1,56 @@
+from __future__ import with_statement
+from pymt import *
+
+picking_buffer = None
+
+class Crayon(MTWidget):
+
+    def __init__(self, **kwargs):
+        super(Crayon, self).__init__(**kwargs)
+        self.model = OBJ('crayon.obj')
+        self.pos = (100,100)
+
+    def on_draw(self):
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_NORMALIZE)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_COLOR_MATERIAL)
+
+        self.draw()
+        self.draw_picking()
+        drawTexturedRectangle(picking_buffer.texture, size=(256,256))
+
+
+    def draw(self):
+        glPushMatrix()
+        glTranslatef(self.x, self.y, 0)
+        glScaled(30,30,30)
+        glRotated(110, 1,0,0)
+        glColor3f(0,1,0)
+        self.model.draw()
+        glPopMatrix()
+
+    def draw_picking(self):
+        with picking_buffer:
+            glDisable(GL_LIGHTING)
+            glDisable(GL_DEPTH_TEST)
+            self.draw()
+
+
+
+
+    def on_touch_move(self, touches, touchID, x, y):
+        print 'setting pos'
+        self.pos = (x,y)
+
+
+
+
+if __name__ == '__main__':
+
+    w = MTWindow()
+    picking_buffer  = Fbo(w.size)
+    c = Crayon()
+    w.add_widget(c)
+    runTouchApp()

4d1a45addea8d91c3c2a37fbe6cffde15e4d13ed
lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-03 14:54:09 +0000

    History first part (up arrow) in widget tester
    git-svn-id: https://pymt.googlecode.com/svn/trunk@573 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/ui/widgettester.py
+++ pymt/ui/widgettester.py
@@ -17,14 +17,31 @@ Enjoy !
 from pymt import *
 
 w = MTWindow()
+
+widgets = {}
+history = []
+count = -1
+
 def on_key_press(symbol, modifiers):
-    if symbol == 65289:
+    global count
+    if symbol == 65362: #up arrow
+        if not keyb.is_active_input:
+           keyb.show_keyboard()
+        count = count + 1
+        if count == len(history):
+            count = 0
+        print count
+        keyb.label = list(reversed(history))[count]
+        
+    if symbol == 65364: #down arrow
+        pass
+        
+    if symbol == 65289: #tab
         if not keyb.is_active_input:
            keyb.show_keyboard()
         else: keyb.hide_keyboard()
 w.push_handlers(on_key_press = on_key_press)
 
-widgets = {}
 keyb = MTTextInput(size = (35,30), font_size = 16, pos = (0, w.height - 30))
 
 def add(name, widget):
@@ -33,13 +50,16 @@ def add(name, widget):
 
 @keyb.event
 def on_text_validate():
+    global count
+    count = -1
     try:
         exec keyb.label
     except Exception, e:
         print 'Error', e
         p = MTPopup(title='Exception occured', content=str(e))
         w.add_widget(p)
-        
+    history.append(keyb.label)
+    print history   
     keyb.label = ''
     keyb.size = (35,30)
     keyb.pos = (0, w.height - 30)

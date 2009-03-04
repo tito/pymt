a99f494eb99b0e044a92458ef55619340793bb8a
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-04 20:57:35 +0000

    settings: update settings example
    git-svn-id: https://pymt.googlecode.com/svn/trunk@589 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/widgets/settings.py
+++ examples/widgets/settings.py
@@ -1,18 +1,6 @@
 from __future__ import with_statement
 from pymt import *
 
-xmldef = '''<?xml version="1.0" encoding="UTF-8"?>
-<MTForm pos="(0,0)"
-layout="factory.get('MTGridLayout')(spacing=20, cols=2, rows=7, uniform_height=True)">
-    <MTFormLabel label="'Fullscreen'" halign="'right'"/>
-    <MTFormCheckbox halign="'left'" id="'chk_fullscreen'"/>
-    <MTFormLabel label="'Show FPS'" halign="'right'"/>
-    <MTFormCheckbox halign="'left'" id="'chk_fps'"/>
-    <MTFormButton label="'Save'"/>
-    <MTFormButton label="'Close'"/>
-</MTForm>
-'''
-
 class MTTabs(MTWidget):
     def __init__(self, **kwargs):
         super(MTTabs, self).__init__(**kwargs)
@@ -23,26 +11,57 @@ class MTTabs(MTWidget):
         self.current = None
         self.tabs = dict()
 
-    def add_widget(self, widget, title):
-        button = MTButton(label=title, size=(120, 40))
+    def add_widget(self, widget, tab=None):
+        if tab is None:
+            if not hasattr(widget, 'tab'):
+                raise Exception('Widget added without tab information')
+            else:
+                tab = widget.tab
+        button = MTButton(label=tab, size=(120, 40))
         button.tab_container = self
         @button.event
         def on_release(touchID, x, y):
-            self.select(title)
+            self.select(tab)
         self.topbar.add_widget(button)
-        self.tabs[title] = (button, widget)
+        self.tabs[tab] = (button, widget)
 
-    def select(self, title):
-        if title not in self.tabs:
+    def select(self, tab):
+        if tab not in self.tabs:
             return
-        button, widget = self.tabs[title]
+        button, widget = self.tabs[tab]
         if self.current:
             self.layout.remove_widget(self.current, do_layout=False)
         self.layout.add_widget(widget, do_layout=False)
         self.current = widget
         self.layout.do_layout()
 
+MTWidgetFactory.register('MTTabs', MTTabs)
 
+xmlsettings = '''<?xml version="1.0" encoding="UTF-8"?>
+<MTTabs>
+    <MTForm tab="'General'"
+        layout="factory.get('MTGridLayout')(spacing=20, cols=2, rows=2, uniform_height=True)">
+        <MTFormLabel label="'Fullscreen'" halign="'right'"/>
+        <MTFormCheckbox halign="'left'" id="'fs_fullscreen'"/>
+        <MTFormLabel label="'Show FPS'" halign="'right'"/>
+        <MTFormCheckbox halign="'left'" id="'fs_fps'"/>
+    </MTForm>
+    <MTForm tab="'Touches'"
+        layout="factory.get('MTGridLayout')(spacing=20, cols=2, rows=2, uniform_height=True)">
+        <MTFormLabel label="'Detection time'" halign="'right'"/>
+        <MTFormInput halign="'left'" id="'fs_double_tap_time'"/>
+        <MTFormLabel label="'Detection distance'" halign="'right'"/>
+        <MTFormInput halign="'left'" id="'fs_double_tap_distance'"/>
+    </MTForm>
+    <MTForm tab="'Tuio'"
+        layout="factory.get('MTGridLayout')(spacing=20, cols=2, rows=2, uniform_height=True)">
+        <MTFormLabel label="'Host'" halign="'right'"/>
+        <MTFormInput halign="'left'" id="'fs_tuio_host'"/>
+        <MTFormLabel label="'Port'" halign="'right'"/>
+        <MTFormInput halign="'left'" id="'fs_tuio_port'"/>
+    </MTForm>
+</MTTabs>
+'''
 
 w = MTWindow()
 
@@ -54,19 +73,28 @@ def action_fps(checked):
     global w
     w.show_fps = checked
 
-xw = XMLWidget(xml=xmldef).children[0]
-chk_fullscreen = getWidgetById('chk_fullscreen')
-chk_fullscreen.push_handlers(on_check=action_fullscreen)
-chk_fps = getWidgetById('chk_fullscreen')
-chk_fps.push_handlers(on_check=action_fps)
+xw = XMLWidget(xml=xmlsettings).children[0]
 
-m = MTTabs()
-m.add_widget(xw, 'Form')
-m.add_widget(MTTextInput(), 'Text input')
-m.add_widget(MTButton(label='plop'), 'Button')
-p = MTScatterPlane()
-p.add_widget(m)
-w.add_widget(p)
+# Connect
+fs_fullscreen = getWidgetById('fs_fullscreen')
+fs_fullscreen.push_handlers(on_check=action_fullscreen)
+fs_fps = getWidgetById('fs_fps')
+fs_fps.push_handlers(on_check=action_fps)
+fs_double_tap_time = getWidgetById('fs_double_tap_time')
+fs_double_tap_distance = getWidgetById('fs_double_tap_distance')
+fs_tuio_host = getWidgetById('fs_tuio_host')
+fs_tuio_port = getWidgetById('fs_tuio_port')
+
+# Set default values
+fs_fullscreen.checked = pymt_config.getboolean('pymt', 'fullscreen')
+fs_fps.checked = pymt_config.getboolean('pymt', 'show_fps')
+fs_double_tap_time.value = pymt_config.get('pymt', 'double_tap_time')
+fs_double_tap_distance.value = pymt_config.get('pymt', 'double_tap_distance')
+fs_tuio_host.value = pymt_config.get('tuio', 'host')
+fs_tuio_port.value = pymt_config.get('tuio', 'port')
+
+# Do tabs
+w.add_widget(xw)
 
 #w.add_widget(MTPopup(width=300, content="Welcome to PyMT Widget Demonstration\nEnjoy this popup !"))
 runTouchApp()

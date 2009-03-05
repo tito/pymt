82a175dc499f0a6d52917d4fe438738414329497
lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-05 02:51:31 +0000

    button matrix improvements
    git-svn-id: https://pymt.googlecode.com/svn/trunk@593 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: lecaude <lecaude@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/ui/widgets/buttonmatrix.py
+++ pymt/ui/widgets/buttonmatrix.py
@@ -14,12 +14,18 @@ class MTButtonMatrix(MTWidget):
 
     def __init__(self,**kwargs):
         kwargs.setdefault('matrix_size', (3,3))
+        kwargs.setdefault('border', 5)
         super(MTButtonMatrix, self).__init__(**kwargs)
-        self.matrix_size = kwargs.get('matrix_size')
-        self.matrix = [[0 for i in range(self.matrix_size[0])] for j in range(self.matrix_size[1])]
-        self.border = 5
-        self.matrix_size = kwargs.get('matrix_size')
-
+        self._matrix_size = kwargs.get('matrix_size')
+        self.border = kwargs.get('border')
+        self.matrix = [[0 for i in range(self._matrix_size[1])] for j in range(self._matrix_size[0])]
+        
+    def _get_matrix_size(self):
+        return self._matrix_size
+    def _set_matrix_size(self, size):
+        self._matrix_size = size
+        self.matrix = [[0 for i in range(self._matrix_size[1])] for j in range(self._matrix_size[0])]
+    matrix_size = property(_get_matrix_size,_set_matrix_size)
 
     def draw_tile(self, i, j):
         if self.matrix[i][j] == 0:
@@ -28,27 +34,31 @@ class MTButtonMatrix(MTWidget):
             set_color(0,0,1,1)
 
         with gx_matrix:
-            glTranslatef(self.width/self.matrix_size[0]*i, self.height/self.matrix_size[1]*j,0)
-            s =  (self.width/self.matrix_size[0]-self.border,self.height/self.matrix_size[1]-self.border)
+            glTranslatef(self.width/self._matrix_size[0]*i, self.height/self._matrix_size[1]*j,0)
+            s =  (self.width/self._matrix_size[0]-self.border,self.height/self._matrix_size[1]-self.border)
             drawRectangle(size=s)
 
 
     def draw(self):
-        for i in range (self.matrix_size[0]):
-            for j in range (self.matrix_size[1]):
+        for i in range (self._matrix_size[0]):
+            for j in range (self._matrix_size[1]):
                 self.draw_tile(i,j)
 
 
     def collide_point(self, x, y):
-        i = x/(self.width/self.matrix_size[0])
-        j = y/(self.height/self.matrix_size[1])
-        return (i,j)
+        i = x/(self.width/self._matrix_size[0])
+        j = y/(self.height/self._matrix_size[1])
+        if i >= self._matrix_size[0] or j >= self._matrix_size[1]:
+            return False # returns false if the click is not within the widget
+        else:
+            return (i,j)
 
     def on_touch_down(self, touches, touchID, x, y):
-        i,j = self.collide_point(x,y)
-        if self.matrix[i][j] == 'down':
-            self.matrix[i][j] = 0
-        else:
-            self.matrix[i][j] = 'down'
+        if self.collide_point(x,y):
+            i,j = self.collide_point(x,y)
+            if self.matrix[i][j] == 'down':
+                self.matrix[i][j] = 0
+            else:
+                self.matrix[i][j] = 'down'
 
 MTWidgetFactory.register('MTButtonMatrix', MTButtonMatrix)

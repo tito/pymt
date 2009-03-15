897144292621a0ccfa1c5f15bf1c7fdd1355d1e7
xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-15 19:05:55 +0000

    New Picture Tutorial
    Tutorials: Added a new pictures tutorial - Background
    panning+rotating+scaling, all movements kinetic in less then 17 lines.
    Pictures: Sample pictures folder, I took just what was on my desktop
    
    
    git-svn-id: https://pymt.googlecode.com/svn/trunk@639 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

---
+++ examples/tutorials/tut3.py
@@ -0,0 +1,17 @@
+import glob
+from pymt import *
+
+w = MTWindow(fullscreen=False)
+
+piclist = glob.glob('./pictures/*.jpg')
+
+k = MTKinetic()
+w.add_widget(k)
+
+p = MTScatterPlane()
+k.add_widget(p)
+
+for pic in piclist:
+    p.add_widget(MTScatterImage(filename=pic))
+
+runTouchApp()

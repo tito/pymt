f30fa8758d25329bd97a22a267662fdc1c286f34
xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-15 05:24:15 +0000

    Added file used in the second tutorial.
    git-svn-id: https://pymt.googlecode.com/svn/trunk@635 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

---
+++ examples/tutorials/tut2.py
@@ -0,0 +1,16 @@
+import random
+from pymt import *
+
+w = MTWindow(fullscreen=False)
+
+b = MTButton(label='Hello, World!', pos=(40, 40), size=(200, 200))
+w.add_widget(b)
+
+l = MTLabel(text='#', font_size=200, pos=(270, 40))
+w.add_widget(l)
+
+@b.event
+def on_press(touchID, x, y):
+    l.text = str(random.randrange(0, 100))
+
+runTouchApp() 

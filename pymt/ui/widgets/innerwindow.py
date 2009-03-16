5cb2e9b193593dc1894ccf68d7061b313e18224e
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-16 22:46:08 +0000

    inner window: replace fbo by stencil test + optim to prevent (fix #37)
    git-svn-id: https://pymt.googlecode.com/svn/trunk@646 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/ui/widgets/innerwindow.py
+++ pymt/ui/widgets/innerwindow.py
@@ -5,7 +5,7 @@ import os
 import pymt
 from pyglet.gl import *
 from ...graphx import gx_matrix, drawRectangle, set_color
-from ...graphx import drawRoundedRectangle, Fbo, drawTexturedRectangle
+from ...graphx import drawRoundedRectangle, drawTexturedRectangle
 from ...vector import matrix_inv_mult
 from rectangle import MTRectangularWidget
 from scatter import MTScatterWidget
@@ -21,44 +21,26 @@ class MTInnerWindow(MTScatterWidget):
 
         self.color=(1,1,1,0.5)
         self.border = 20
-
-        self.needs_redisplay = True
-        self.window_fbo = Fbo(size=self.size)
+        self.__w = self.__h = 0
 
         self.container = MTRectangularWidget(pos=(0,0),size=self.size)
         super(MTInnerWindow, self).add_widget(self.container)
 
-        self.setup_controlls()
-        self.locked = False
+        self.setup_controls()
 
 
-
-    def setup_controlls(self):
-        self.controlls = MTBoxLayout()
-        button_pause = MTImageButton(filename=iconPath+'pause.png', scale=0.5)
-        button_pause.push_handlers(on_release=self.toggle_locked)
-        self.controlls.add_widget(button_pause)
+    def setup_controls(self):
+        self.controls = MTBoxLayout()
 
         button_fullscreen = MTImageButton(filename=iconPath+'fullscreen.png',scale=0.5)
         button_fullscreen.push_handlers(on_release=self.fullscreen)
-        self.controlls.add_widget(button_fullscreen)
+        self.controls.add_widget(button_fullscreen)
 
-        button_close = MTImageButton(filename=iconPath+'stop.png',scale=0.5)
+        button_close = MTImageButton(filename=iconPath+'stop.png', scale=0.5)
         button_close.push_handlers(on_release=self.close)
-        self.controlls.add_widget(button_close)
+        self.controls.add_widget(button_close)
 
-
-        self.controlls.x, self.controlls.y = self.width/2 - button_pause.width*3/2, -button_pause.height*1.7
-
-    def toggle_locked(self, touchID=None, x=0, y=0 ):
-        if self.locked:
-            super(MTInnerWindow, self).add_widget(self.container)
-            self.locked = False
-            self.resize_fbo(self.width, self.height, force=True)
-        else:
-            super(MTInnerWindow, self).remove_widget(self.container)
-            self.locked = True
-
+        self.controls.pos = self.width/2, -button_fullscreen.height*1.7
 
     def fullscreen(self,touchID=None, x=0, y=0):
         root_win = self.parent.get_parent_window()
@@ -76,44 +58,34 @@ class MTInnerWindow(MTScatterWidget):
     def get_parent_window(self):
         return self.container
 
-    def on_resize(self, w,h):
-        if not self.locked:
-            self.resize_fbo(w,h)
-        for button in self.controlls.children:
+    def on_resize(self, w, h):
+        # XXX remove this optim, and do it in scatter widget !
+        if self.__w == w and self.__h == h:
+            return
+        self.__w, self.__h = w, h
+        for button in self.controls.children:
             button.scale = 0.5/self.get_scale_factor()
-        self.controlls.x, self.controlls.y = self.width/2 - self.controlls.children[0].width*3/2, -self.controlls.children[0].height*1.7
+        self.controls.pos = self.width/2 - self.controls.children[0].width*3/2, -self.controls.children[0].height*1.7
         for child in self.container.children:
             child.on_resize(w,h)
 
-    def resize_fbo(self, w, h, force=False):
-        if abs(w-self.window_fbo.size[0]) > 50 or force:
-            self.window_fbo = Fbo(size=(w,h))
-            self.container.size = (w,h)
-            self.needs_redisplay = True
-
     def on_touch_down(self, touches, touchID, x,y):
         lx,ly = super(MTInnerWindow, self).to_local(x,y)
-        if self.controlls.dispatch_event('on_touch_down', touches, touchID, lx, ly):
+        if self.controls.dispatch_event('on_touch_down', touches, touchID, lx, ly):
             return True
         return super(MTInnerWindow, self).on_touch_down( touches, touchID, x,y)
 
     def on_touch_move(self, touches, touchID, x,y):
         lx,ly = super(MTInnerWindow, self).to_local(x,y)
-        if self.controlls.dispatch_event('on_touch_move', touches, touchID, lx, ly):
+        if self.controls.dispatch_event('on_touch_move', touches, touchID, lx, ly):
             return True
         return super(MTInnerWindow, self).on_touch_move( touches, touchID, x,y)
 
     def on_touch_up(self, touches, touchID, x,y):
         lx,ly = super(MTInnerWindow, self).to_local(x,y)
-        if self.controlls.dispatch_event('on_touch_up', touches, touchID, lx, ly):
+        if self.controls.dispatch_event('on_touch_up', touches, touchID, lx, ly):
             return True
-        #force correct resize on touch up...this way we guarantee its always exactly correct when resie is done
-        if super(MTInnerWindow, self).on_touch_up( touches, touchID, x,y):
-            if not self.locked:
-                scale = self.get_scale_factor()
-                self.resize_fbo(int(self.width*scale), int(self.height*scale), force=True)
-            return True
-
+        return super(MTInnerWindow, self).on_touch_up( touches, touchID, x,y)
 
     def to_local(self,x,y):
         self.new_point = matrix_inv_mult(self.transform_mat, (x,y,0,1)) * self.get_scale_factor()
@@ -126,32 +98,37 @@ class MTInnerWindow(MTScatterWidget):
         bottom,top = -scaled_border,self.height+scaled_border*2
         if local_coords[0] > left and local_coords[0] < right \
            and local_coords[1] > bottom and local_coords[1] < top:
-            self.needs_redisplay = True
             return True
         else:
             return False
-
 
     def draw(self):
         set_color(*self.color)
         scaled_border = int(self.border * (1.0/self.get_scale_factor()))
         drawRoundedRectangle((-scaled_border, -scaled_border), (self.width+scaled_border*2, self.height+scaled_border*2))
-        with gx_matrix:
-            drawRectangle(((self.width/2)-(scaled_border*2.5), -scaled_border), (scaled_border*5, -scaled_border*1.2))
-
+        drawRectangle(((self.width/2)-(scaled_border*2.5), -scaled_border), (scaled_border*5, -scaled_border*1.2))
 
     def on_draw(self):
-        if not self.locked:
-            self.window_fbo.bind()
-            self.container.dispatch_event('on_draw')
-            self.window_fbo.release()
-            self.needs_redisplay = False
-
         with gx_matrix:
             glMultMatrixf(self.transform_mat)
+
             self.draw()
-            drawTexturedRectangle(self.window_fbo.texture, (0,0), self.size)
-            if self.locked:
-                set_color(0.5,0.5,1, 0.3)
-                drawRectangle((0,0), self.size)
-            self.controlls.dispatch_event('on_draw')
+
+            # enable stencil test
+            glClearStencil(0)
+            glClear(GL_STENCIL_BUFFER_BIT)
+            glEnable(GL_STENCIL_TEST)
+            glStencilFunc(GL_NEVER, 0x0, 0x0)
+            glStencilOp(GL_INCR, GL_INCR, GL_INCR)
+            drawRectangle((0, 0), size=self.size)
+
+            # draw inner content
+            glStencilFunc(GL_EQUAL, 0x1, 0x1)
+            glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP)
+            self.container.dispatch_event('on_draw')
+            glDisable(GL_STENCIL_TEST)
+
+            self.controls.dispatch_event('on_draw')
+
+
+

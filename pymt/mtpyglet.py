59bff67d18708be22459ed7d47c86f3da1f5f1a0
xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-17 01:12:45 +0000

    mtpyglet: Removed motcalc, an old dead method that never worked in the first place.
    git-svn-id: https://pymt.googlecode.com/svn/trunk@647 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- pymt/mtpyglet.py
+++ pymt/mtpyglet.py
@@ -40,19 +40,10 @@ class Tuio2DCursor(object):
         self.double_tap_time = 0
         self.no_event = False
         self.depack(args)
-        self.motcalc()
 
     def move(self, args):
         self.oxpos, self.oypos = self.xpos, self.ypos
         self.depack(args)
-        self.motcalc()
-
-    def motcalc(self):
-        #NOTE: This does not work with the mouse, but with a real Tuio stream it does
-        '''Calculates the relative movement if the tracker is not providing it'''
-        '''Ported from touchpy'''
-        self.xmot = self.xpos - self.oxpos
-        self.ymot = self.ypos - self.oypos
 
     def depack(self, args):
         if len(args) < 5:
@@ -76,13 +67,6 @@ class Tuio2DObject(object):
     def move(self, args):
         self.oxpos, self.oypos = self.xpos, self.ypos
         self.depack(args)
-        self.motcalc()
-
-    def motcalc(self):
-        '''Calculates the relative movement if the tracker is not providing it'''
-        '''Ported from touchpy'''
-        self.xmot = self.xpos - self.oxpos
-        self.ymot = self.ypos - self.oypos
 
     def depack(self, args):
         if len(args) < 5:

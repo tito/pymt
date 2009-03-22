14723b6b87f84005166f6532cbb1c73df2fa590a
xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-22 05:02:05 +0000

    Added an example to widgets for KineticList.
    git-svn-id: https://pymt.googlecode.com/svn/trunk@687 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

---
+++ examples/widgets/kinetic.py
@@ -0,0 +1,16 @@
+from pymt import *
+
+mms = MTWindow()
+
+k = MTKineticList(pos=(200, 200), size=(400, 400), w_limit=3)
+mms.add_widget(k)
+
+d = range(0, 50)
+for x in d:
+    k.add(MTKineticItem(label=str(x)), x)
+
+@k.event
+def on_delete(item, callback):
+    d.remove(callback)
+
+runTouchApp()

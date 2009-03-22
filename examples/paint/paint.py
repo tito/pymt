2cce81402582a7777193db7a7b83f6631126ff43
txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-03-22 13:35:39 +0000

    paint: enhance paint app (touchcamp idea!)
    git-svn-id: https://pymt.googlecode.com/svn/trunk@695 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: txprog <txprog@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/paint/paint.py
+++ examples/paint/paint.py
@@ -1,5 +1,6 @@
 from pymt import *
 from pyglet.gl import *
+from glob import glob
 
 # PYMT Plugin integration
 IS_PYMT_PLUGIN = True
@@ -8,8 +9,87 @@ PLUGIN_AUTHOR = 'Thomas Hansen'
 PLUGIN_EMAIL = 'thomas.hansen@gmail.com'
 PLUGIN_DESCRIPTION = 'This is a simple paint canvas.'
 
-#import psyco
-#psyco.profile(0.7)
+class PaintBrushLayout(MTBoxLayout):
+    def draw(self):
+        set_color(0, 0, 0, 1)
+        drawRectangle(self.pos, self.size)
+
+
+
+class MTPaintColorPicker(MTWidget):
+    def __init__(self, **kwargs):
+        kwargs.setdefault('min', 0)
+        kwargs.setdefault('max', 255)
+        kwargs.setdefault('target', [])
+        super(MTPaintColorPicker, self).__init__(**kwargs)
+        self.targets = kwargs.get('targets')
+        self.sliders = [ MTSlider(min=kwargs.get('min'), max=kwargs.get('max'), size=(30,200), slidercolor=(1,0,0,1)),
+                        MTSlider(min=kwargs.get('min'), max=kwargs.get('max'), size=(30,200), slidercolor=(0,1,0,1)),
+                        MTSlider(min=kwargs.get('min'), max=kwargs.get('max'), size=(30,200), slidercolor=(0,0,1,1)),
+                        MTSlider(min=kwargs.get('min'), max=kwargs.get('max'), size=(30,200), slidercolor=(1,1,1,.7)),
+                        MTSlider(min=kwargs.get('min'), max=kwargs.get('max'), size=(30,200), slidercolor=(1,1,1,.7)) ]
+        for slider in self.sliders:
+            slider.value = 200
+        self.update_color()
+        self.touch_positions = {}
+        self.brush_size = 10
+
+    def draw(self):
+        set_color(0.2,0.2,0.2,0.5)
+        drawRectangle(pos=(self.x, self.y), size=(self.width,self.height))
+
+        set_color(*self.current_color)
+        drawRectangle(pos=(self.x+10, self.y+220), size=(110,60))
+
+        pos = self.x + 170, self.y + 250
+        paintLine(pos+pos)
+
+
+        for i in range(len(self.sliders)):
+            self.sliders[i].x = 10 + self.x + i*40
+            self.sliders[i].y = 10 + self.y
+            self.sliders[i].draw()
+
+    def update_color(self):
+        r = self.sliders[0].value/255.0
+        g = self.sliders[1].value/255.0
+        b = self.sliders[2].value/255.0
+        a = self.sliders[3].value/255.0
+        for w in self.targets:
+            w.color = (r,g,b,a)
+        self.current_color = (r,g,b,a)
+        self.brush_size = (self.sliders[4].value/4) + 1
+        set_brush_size(self.brush_size)
+
+    def on_touch_down(self, touches, touchID, x, y):
+        for s in self.sliders:
+            if s.on_touch_down(touches, touchID, x, y):
+                self.update_color()
+                return True
+
+        if self.collide_point(x,y):
+            self.touch_positions[touchID] = (x, y, touchID)
+            return True
+
+    def on_touch_move(self, touches, touchID, x, y):
+        for s in self.sliders:
+            if s.on_touch_move(touches, touchID, x, y):
+                self.update_color()
+                return True
+        if touchID in self.touch_positions:
+            self.x += x - self.touch_positions[touchID][0]
+            self.y += y - self.touch_positions[touchID][1]
+            self.touch_positions[touchID] = (x,y,touchID)
+            return True
+
+    def on_touch_up(self, touches, touchID, x, y):
+        for s in self.sliders:
+            if s.on_touch_up(touches, touchID, x, y):
+                self.update_color()
+                return True
+        if touchID in self.touch_positions:
+            del self.touch_positions[touchID]
+
 
 
 class Canvas(MTWidget):
@@ -17,12 +97,9 @@ class Canvas(MTWidget):
         MTWidget.__init__(self, pos=pos, size=size)
         self.touch_positions = {}
         self.fbo = Fbo((self.width, self.height), push_viewport=False)
-        self.bgcolor = (0.8,0.8,0.7,1.0)
+        self.bgcolor = (0,0,0,1)
         self.color = (0,1,0,1.0)
-        try:
-            set_brush('../touchtracer/particle.png')
-        except:
-            set_brush('touchtracer/particle.png')
+        set_brush('../paint/brushes/brush_particle.png')
         self.clear()
 
     def clear(self):
@@ -33,12 +110,11 @@ class Canvas(MTWidget):
         self.fbo.release()
 
     def draw(self):
-        glColor4f(1,1,1,1)
+        set_color(1,1,1,1)
         drawTexturedRectangle( self.fbo.texture, size=(self.width, self.height))
 
     def on_resize(self, w, h):
         del self.fbo
-        print "resizing fbo"
         self.fbo = Fbo((w, h), push_viewport=False)
 
     def on_touch_down(self, touches, touchID, x, y):
@@ -62,6 +138,11 @@ class Canvas(MTWidget):
         if self.touch_positions.has_key(touchID):
             del self.touch_positions[touchID]
 
+def update_brush(brush, touchID, x, y):
+    set_brush(brush)
+
+def clear_canvas(canvas, touchID, x, y):
+    canvas.clear()
 
 def pymt_plugin_activate(root, ctx):
 
@@ -71,8 +152,21 @@ def pymt_plugin_activate(root, ctx):
     root.push_handlers(on_resize=resizeCanvas)
 
     root.add_widget(ctx.canvas)
-    ctx.slider = MTColorPicker(size=(130,290), targets=[ctx.canvas])
+    ctx.slider = MTPaintColorPicker(size=(220,290), targets=[ctx.canvas])
     root.add_widget(ctx.slider)
+
+    ctx.btnclear = MTButton(label='Clear', size=(50,50), pos=(0, root.height-50))
+    ctx.btnclear.push_handlers(on_press=curry(clear_canvas, ctx.canvas))
+    root.add_widget(ctx.btnclear)
+
+    ctx.brushes = PaintBrushLayout(pos=(300, 0))
+    for brush in glob('../paint/brushes/*.png'):
+        button = MTImageButton(filename=brush)
+        button.push_handlers(on_press=curry(update_brush, brush))
+        ctx.brushes.add_widget(button)
+    root.add_widget(ctx.brushes)
+
+
 
 def pymt_plugin_deactivate(root, ctx):
     root.remove_widget(ctx.canvas)

742e6b5820581dabbcc3b7c5ce2421f89be3ca30
xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>
2009-04-18 06:09:47 +0000

    FlipSide: It Plays Music!
    Woohoo!  It can take a directory of music files, show them in a list(in order they appear on the CD), and let you tap to play them!
    
    It occaisionally crashes when switching songs rapidly, this will be invenstigated further at a later time.
    
    git-svn-id: https://pymt.googlecode.com/svn/trunk@778 0ace1f6a-820f-11dd-b6e4-1fb4a74862d0
    
    committer: xelapond <xelapond@0ace1f6a-820f-11dd-b6e4-1fb4a74862d0>

--- examples/sandbox/flipside/flipside.py
+++ examples/sandbox/flipside/flipside.py
@@ -1,16 +1,35 @@
 import os
 from pymt import *
+from pymt.logger import pymt_logger
 import pyglet
-from mutagen.id3 import ID3 
+from mutagen import id3
 from cStringIO import StringIO
 from mutagen import oggvorbis as ogg
 import glob
 
+def get_comments(file):
+    ext = file.split('/').pop().split('.').pop().lower()
+    if ext == 'ogg':
+        return ogg.Open(file)
+    elif ext == 'mp3':
+        return id3.Open(file)
+    else:
+        pymt_logger.warning('File "%s" is not recognized by FlipSide', file)
+        return None
+
 class PlayManager(MTScatterWidget):
     def __init__(self, **kwargs):
+        super(PlayManager, self).__init__(**kwargs)
         self.player = pyglet.media.Player()
-        
 
+    def play(self, file):
+        self.player.seek(0)
+        source = pyglet.media.load(file, streaming=True)
+        self.player.queue(source)
+        if self.player.playing:
+            self.player.next()
+        else:
+            self.player.play()
 
 class KineticSong(MTKineticItem):
     def __init__(self, **kwargs):
@@ -25,16 +44,20 @@ class SongList(MTKineticList):
         kwargs.setdefault('pos', (0, 0))
         kwargs.setdefault('size', (400, 400))
         super(SongList, self).__init__(**kwargs)
-        #self.player = kwargs.get('player')
+        self.player = kwargs.get('player')
 
-    def add_song(self, path):
-        comments = ogg.Open(path)
-        self.add(KineticSong(comments=comments))
+    def add_song(self, path, comments):
+        print path
+        self.add(KineticSong(comments=comments, path=path))
         self.sort()
 
     def sort(self):
         self.pchildren.sort(lambda *l: (lambda x, y: y - x)(*[int(i.comments['tracknumber'].pop()) for i in l]))
         self.children = self.pchildren
+
+    def on_press(self, child, callback):
+        print child
+        self.player.play(child.path)
 
 class AlbumFloater(MTScatterImage):
     def __init__(self, **kwargs):
@@ -44,7 +67,7 @@ class AlbumFloater(MTScatterImage):
         self.scale = 400.0/float(self.image.width)
         self.image.scale = self.scale
 
-        self.list = SongList()
+        self.list = SongList(player=kwargs.get('player'))
         self.add_widget(self.list, 'back')
 
     def on_touch_down(self, touches, touchID, x, y):
@@ -69,6 +92,9 @@ class AlbumFloater(MTScatterImage):
 #MUSIC_DIR = '/home/alex/Music'
 #music_tree = os.walk(MUSIC_DIR)
 
+##EVERYTHING PAST HERE IS TESTING
+#It will be replaces(probably tomorrow) with real code.
+
 w = MTWindow(fullscreen=False, size=(600, 600))
 
 k = MTKinetic()
@@ -77,18 +103,17 @@ w.add_widget(k)
 p = MTScatterPlane()
 k.add_widget(p)
 
-s = AlbumFloater(filename='/home/alex/Desktop/cover.jpg')
+x = PlayManager()
+
+s = AlbumFloater(filename='/home/alex/Desktop/cover.jpg', player=x)
 
 s.add_widget(MTButton(), side='back')
 
-for file in glob.glob('/home/alex/Music/Pink Floyd/Wish You Were Here/*.ogg'):
-    print file
-    s.list.add_song(file)
-
-#s.sort()
-
-
+for file in glob.glob('/home/alex/Music/Pink Floyd/Wish You Were Here/*.*'):
+    print get_comments(file)
+    s.list.add_song(file, get_comments(file))
 
 p.add_widget(s)
+p.add_widget(x)
 
 runTouchApp()

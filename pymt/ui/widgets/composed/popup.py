f14f3541155c49f9e0f8f71bfcf100f24e87ec80
Mathieu Virbel <tito@bankiz.org>
2009-06-05 10:49:04 +0200

    popup: add cls for theming and example

--- pymt/ui/widgets/composed/popup.py
+++ pymt/ui/widgets/composed/popup.py
@@ -32,16 +32,14 @@ class MTPopup(MTModalWindow):
             padding=10, spacing=10, orientation='vertical',
             invert_y=True, uniform_width=True
         ))
-        self.submit = MTFormButton(label='OK')
+        self.submit = MTFormButton(label='OK', cls='popup-submit')
         self.submit.push_handlers(on_release=self.action_close_popup)
-        self.form.add_widget(MTFormLabel(label=self.title, halign='left',
-                                         font_style='bold', font_size=16))
-        self.form.add_widget(MTFormLabel(label=self.content, halign='left',
-                                         multiline=True, width=self.width))
+        self.form.add_widget(MTFormLabel(label=self.title, cls='popup-title'))
+        self.form.add_widget(MTFormLabel(label=self.content, cls='popup-content'))
         self.form.add_widget(self.submit)
         super(MTPopup, self).add_widget(self.form)
 
-    def action_close_popup(self, touchID, x, y):
+    def action_close_popup(self, *largs):
         self.parent.remove_widget(self)
 
     def add_widget(self, widget):

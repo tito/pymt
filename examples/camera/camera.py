f2cc0bcb25a303c0c7ae275f758d81b1ed2c405b
Thomas Hansen <thomas.hansen@gmail.com>
2009-06-21 00:28:13 -0500

    made changes to gstreamer camera also.  gstreamer camera has bug when changing resolution at runtime.  i think its because camera isnt released and then acquired properly again.  maybe there is a way of only changing part of gstremaer pipeline at runtime or actual freeing of resources, will try to read more and find out.

--- examples/camera/camera.py
+++ examples/camera/camera.py
@@ -23,19 +23,22 @@ class MTCameraBase(MTWidget):
     '''Abstract Camer Widget class.  concrete camera widget classes must implement initializaation and frame capturing to buffer that can be uploaded to gpu'''
 
     def __init__(self, **kwargs):
-
         kwargs.setdefault('stopped', False )
         kwargs.setdefault('resolution', (640,480))
-        kwargs.setdefault('capture_src', 0)
+        kwargs.setdefault('video_src', 0)
 
         self.stopped        = kwargs['stopped']
         self._resolution    = kwargs['resolution']
-        self._capture_src   = kwargs['capture_src']
+        self._video_src   = kwargs['video_src']
         self.capture_device = None
         kwargs.setdefault('size', self._resolution )
 
         super(MTCameraBase, self).__init__(**kwargs)
         self.init_camera()
+
+        if not self.stopped:
+            self.start_capture()
+
 
     def _set_resolution(self, res):
         self._resolution = res
@@ -44,26 +47,27 @@ class MTCameraBase(MTWidget):
         return self._resolution
     resolution = property(_get_resolution, _set_resolution, doc='resolution: resolution of camera capture')
 
-
-    def _set_capture_src(self, src):
-        self._capture_src = src
+    def _set_video_src(self, src):
+        self._video_src = src
         self.init_camera()
-    def _get_capture_src(self):
-        return self._capture_src
-    capture_src = property(_get_capture_src, _set_capture_src, doc='resolution: resolution of camera capture')
+    def _get_video_src(self):
+        return self._video_src
+    video_src = property(_get_video_src, _set_video_src, doc='resolution: resolution of camera capture')
 
 
     def init_camera(self):
         pymt_logger.exception('MTCameraBase is an abstract class.  Use a Sub class providing a specific camera interface instead!')
 
-    def start_capture(self):
-        pymt_logger.exception('MTCameraBase is an abstract class.  Use a Sub class providing a specific camera interface instead!')
-
-    def stop_capture(self):
-        pymt_logger.exception('MTCameraBase is an abstract class.  Use a Sub class providing a specific camera interface instead!')
-
     def capture_frame(self):
         pymt_logger.exception('MTCameraBase is an abstract class.  Use a Sub class providing a specific camera interface instead!')
+
+
+    def start_capture(self):
+        self.stopped = False
+        self.capture_frame()
+
+    def stop_capture(self):
+        self.stopped = True
 
 
     def copy_buffer_to_gpu(self):
@@ -98,7 +102,7 @@ class MTOpenCVCamera(MTCameraBase):
 
     :Dependencies:
         'openCV':
-            This Widget relies on openCV to acquire images from a great variety of webcams with cross platform support
+            This WebCam Widget relies on openCV to acquire images from a great variety of webcams with cross platform support
             You will need to have opencv and python bindings installed.
 
             You can get OpenCV at http://sourceforge.net/projects/opencvlibrary/
@@ -111,39 +115,36 @@ class MTOpenCVCamera(MTCameraBase):
             They should work if you just have openCV installed as a C library
 
     :Parameters:
-        `camera_id` : int, default is 0
-            Label of button
+        `video_src` : int, default is 0
+            camera identifier to attempt capturing from.  OpenCV enumerates capyure devices starting at 0 in integers.
         `size` : tuple (int, int)
             size at which the image is drawn.  if no size is specified, it defaults to resolution of the camera image
+        `resolution` : tuple (int, int)
+            resolution which will be requested for each frame from teh camera.  if the resolution is not available
+            self.resolution will be set to a default value acquired from teh camera device.
     '''
 
-    def __init__(self, **kwargs):
-        super(MTOpenCVCamera, self).__init__(**kwargs)
-
     def init_camera(self):
-        if self.capture_device:
-            del self.capture_device
+        if not self.capture_device:
+            self.capture_device = cvCaptureFromCAM(self.video_src)
 
         try:
-            self.capture_device = cvCaptureFromCAM(self.capture_src)
             cvSetCaptureProperty (self.capture_device, CV_CAP_PROP_FRAME_WIDTH,  self.resolution[0])
             cvSetCaptureProperty (self.capture_device, CV_CAP_PROP_FRAME_HEIGHT, self.resolution[1])
+            self.frame  = cvQueryFrame( self.capture_device )
+            if not int(self.frame.width) == self.resolution[0]:
+                raise Exception('Resolution Not supported')
         except:
-            self.capture_device = cvCaptureFromCAM(self.capture_src)
-            w = cvGetCaptureProperty (self.capture_device, CV_CAP_PROP_FRAME_WIDTH)
-            h = cvGetCaptureProperty (self.capture_device, CV_CAP_PROP_FRAME_HEIGHT)
-            pymt_logger.exception('Warning: Camera resolution %s not possible!  Defaulting to %s.' % (self.resolution, (w,h)) )
+            w = int(cvGetCaptureProperty (self.capture_device, CV_CAP_PROP_FRAME_WIDTH))
+            h = int(cvGetCaptureProperty (self.capture_device, CV_CAP_PROP_FRAME_HEIGHT))
+            self.frame  = cvQueryFrame( self.capture_device )
+            pymt_logger.warning('Warning: Camera resolution %s not possible!  Defaulting to %s.' % (self.resolution, (w,h)) )
             self._resolution = (w,h)
 
         self.frame_texture  = Texture.create(*self.resolution)
         self.frame_texture.tex_coords = (1,1,0,  0,1,0,  0,0,0, 1,0,0)
 
-    def start_capture(self):
-        self.stopped = False
-        self.capture_frame()
 
-    def stop_capture(self):
-        self.stopped = True
 
     def capture_frame(self):
         try:
@@ -157,7 +158,7 @@ class MTOpenCVCamera(MTCameraBase):
             self.copy_buffer_to_gpu()
 
         except Exception, e:
-            print "Couldn't get Image from Camera!", e
+            pymt_logger.exception("Couldn't get Image from Camera!"+ str(e))
 
 
 
@@ -166,15 +167,11 @@ class MTOpenCVCamera(MTCameraBase):
 
 
 
-"""
 
 
-class MTCameraWidgetGST(_CameraWidget):
-Comment by txprog, Yesterday (17 hours ago):
 
-- Remove Widget in name
-- Support parameter change at runtime
 
+class MTGstCamera(MTCameraBase):
     '''MTCameraWidget is a widget that gets images from a webcam and draws the image to a texture and the screen.
 
         This camera widget uses gstreamer instead of opencv to aquire images.
@@ -186,38 +183,50 @@ Comment by txprog, Yesterday (17 hours ago):
 
     :Parameters:
         `video_src` : str, default is 'v4l2src'
-            other tested options are: 'dc1394src' for firewire dc camera (e.g. firefly MV)
+            other tested options are: 'dc1394src' for firewire dc camera (e.g. firefly MV).  any gstreamer video source should potentially work.
+            Theoretically a longer string using "!" can be used describing the first part of a gstreamer pipeline.
+        `size` : tuple (int, int)
+            size at which the image is drawn.  if no size is specified, it defaults to resolution of the camera image
         `resolution` : tuple (int, int)
             resolution to try to request from the camera. used in the gstreamer pipeline by forcing teh appsink caps to this resolution.
             if the camera doesnt support the resolution a negotiation error might be thrown.
     '''
 
     def __init__(self, **kwargs):
-        kwargs.setdefault('res', (640,480))
-        kwargs.setdefault('size', kwargs['res'])
         kwargs.setdefault('video_src', 'v4l2src') #other options: dc1394src, etc..
-        self.resolution = kwargs['res']
-        self.video_src = kwargs['video_src']
-        super(MTCameraWidgetGST, self).__init__(**kwargs)
+        self.pipeline = None
+        super(MTGstCamera, self).__init__(**kwargs)
+
+    #TODO:  This does not work when camera resolution is resized at runtime...there must be some other way to release the camera?
+    def init_camera(self):
+        if self.pipeline:
+            self.pipeline = None
 
         GL_CAPS = "video/x-raw-rgb,width=%d,pixel-aspect-ratio=1/1,red_mask=(int)0xff0000,green_mask=(int)0x00ff00,blue_mask=(int)0x0000ff" % (self.resolution[0])
-        pipeline = gst.parse_launch("%s ! ffmpegcolorspace ! appsink name=camera_sink emit-signals=True caps=%s" % (self.video_src, GL_CAPS) )
-        self.camera_sink = pipeline.get_by_name('camera_sink')
-        pipeline.set_state(gst.STATE_PLAYING)
-
-        self.frame = self.camera_sink.emit('pull-buffer')
-        self.frame_texture  = Texture.create(640,480)
+        self.pipeline = gst.parse_launch("%s ! ffmpegcolorspace ! appsink name=camera_sink emit-signals=True caps=%s" % (self.video_src, GL_CAPS) )
+        self.camera_sink = self.pipeline.get_by_name('camera_sink')
+        self.frame_texture  = Texture.create(*self.resolution)
         self.frame_texture.tex_coords = (1,1,0,  0,1,0,  0,0,0, 1,0,0)
 
+        if self.camera_sink and not self.stopped:
+            self.start_capture()
 
-    def captureFrame(self):
+    def start_capture(self):
+        self.stopped = False
+        self.pipeline.set_state(gst.STATE_PLAYING)
+        self.capture_frame()
+
+    def stop_capture(self):
+        self.stopped = True
+        self.pipeline.set_state(gst.STATE_PAUSED)
+
+    def capture_frame(self):
         self.frame = self.camera_sink.emit('pull-buffer')
         self.format = GL_RGB
 
         self.buffer = self.frame.data
-        self.copy_frame_to_gpu()
+        self.copy_buffer_to_gpu()
 
-"""
 
 
 
@@ -225,7 +234,7 @@ Comment by txprog, Yesterday (17 hours ago):
 
 if __name__ == "__main__":
     w = MTWindow()
-    c = MTOpenCVCamera(resolution=(640,480))
+    c = MTGstCamera()
     s = MTScatterWidget(size=c.size)
     s.add_widget(c)
     w.add_widget(s)

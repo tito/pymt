0ef33802dc8ef89e12bb065f2f636222f2e1ff56
Mathieu Virbel <txprog@gmail.com>
2009-06-26 00:02:22 +0200

    svgbutton: add MTSvgButton

--- pymt/ui/widgets/svg.py
+++ pymt/ui/widgets/svg.py
@@ -1,7 +1,7 @@
 '''
 SVG widget: widget that display an svg image
 '''
-__all__ = ['MTSvg']
+__all__ = ['MTSvg', 'MTSvgButton']
 
 import os
 import squirtle
@@ -9,6 +9,7 @@ import pymt
 from ...logger import pymt_logger
 from ..factory import MTWidgetFactory
 from widget import MTWidget
+from button import MTButton
 
 class MTSvg(MTWidget):
     '''Render an svg image
@@ -22,6 +23,43 @@ class MTSvg(MTWidget):
         if kwargs.get('filename') is None:
             raise Exception('No filename given to MTSvg')
         super(MTSvg, self).__init__(**kwargs)
+        self.filename = kwargs.get('filename')
+        self.size = (self.svg.width, self.svg.height)
+
+    def draw(self, **kwargs):
+        self.svg.draw(self.x, self.y, **kwargs)
+
+    def _get_filename(self):
+        return self._filename
+    def _set_filename(self, filename):
+		# TODO remove this ugly code, improve loader for this
+        try:
+            pymt_logger.debug('loading %s' % filename)
+            self.svg = squirtle.SVG(filename)
+        except Exception, e:
+            try:
+                svgpath = os.path.join(os.path.dirname(pymt.__file__), 'data/icons/svg')
+                pymt_logger.exception('unable to load %s' % filename)
+                pymt_logger.warning('trying %s' % (svgpath + filename))
+                self.svg = squirtle.SVG(os.path.join(svgpath, filename))
+            except Exception, e:
+                pymt_logger.exception('unable to load file %s' % filename)
+        self._filename = filename
+        self.size = (self.svg.width, self.svg.height)
+    filename = property(_get_filename, _set_filename)
+
+class MTSvgButton(MTButton):
+    '''Render an svg image
+
+    :Parameters:
+        `filename` : str
+            Filename of image
+    '''
+    def __init__(self, **kwargs):
+        kwargs.setdefault('filename', None)
+        if kwargs.get('filename') is None:
+            raise Exception('No filename given to MTSvgButton')
+        super(MTSvgButton, self).__init__(**kwargs)
         self.filename = kwargs.get('filename')
         self.size = (self.svg.width, self.svg.height)
 

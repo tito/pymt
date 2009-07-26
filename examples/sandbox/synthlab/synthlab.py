efd8a7fa96c037745b974d7f94402434fbcfb3e3
Nathanaël Lécaudé <Flipo>
2009-07-26 00:40:06 -0400

    examples: updated synthlab

--- examples/sandbox/synthlab/synthlab.py
+++ examples/sandbox/synthlab/synthlab.py
@@ -1,29 +1,34 @@
 from __future__ import with_statement
 from pymt import *
+import os
 
-class Line(MTWidget):
-    def __init__(self, **kwargs):
-        super(Line,self).__init__(**kwargs)
-        
-    def draw(self):
-        set_color(1,1,1,1)
-        drawLine([0,0,500,500], width = 5)
- 
 class Workspace(MTScatterPlane):
     def __init__(self, **kwargs):
         super(Workspace,self).__init__( **kwargs)
-
-class LabIcon(MTSvg):
+        self.modules = [Module(filename = 'sl-addSynth+.svg'),
+                        Module(filename = 'sl-speaker.svg'),
+                        Module(filename = 'sl-distort+.svg')]
+        for m in self.modules:
+            self.add_widget(m)
+        
+class Module(MTSvg):
     def __init__(self, **kwargs):
-        super(LabIcon,self).__init__(**kwargs)
+        super(Module,self).__init__(**kwargs)
         self.touchstarts = [] # only react to touch input that originated on this widget
         self.mode = 'move'
+        self.drag_x = 0
+        self.drag_y = 0
+        self.connected_modules = []
         
-    #def draw(self):
-        #super(LabIcon, self).draw()
-        #with gx_matrix:
-            #drawLine([0,0,500,500])
- 
+    def draw(self):
+        if self.mode == 'draw_connection':
+            set_color(1,1,1,1)
+            drawLine([self.x + self.width / 2, self.y,self.drag_x,self.drag_y], width = 1)
+        set_color(1,1,1,1)
+        for module in self.connected_modules:
+            drawLine([self.x + self.width / 2, self.y, module.x + self.width / 2 , module.y + module.height], width = 1)
+        super(Module, self).draw()
+
     def on_touch_down(self, touch):
         if self.collide_point(touch.x,touch.y):
             self.touchstarts.append(touch.id)
@@ -31,7 +36,12 @@ class LabIcon(MTSvg):
             self.first_y = touch.y
             self.first_pos_x = self.x
             self.first_pos_y = self.y
-            self.first_width = self.width
+            if touch.y < self.y + 20:
+                self.mode = 'draw_connection'
+                self.drag_x = touch.x
+                self.drag_y = touch.y
+            else:
+                self.mode = 'move'
             return True
         
     def on_touch_move(self, touch):
@@ -41,23 +51,27 @@ class LabIcon(MTSvg):
             if self.mode == 'move':
                 self.x = self.first_pos_x + delta_x
                 self.y = self.first_pos_y + delta_y
+            if self.mode == 'draw_connection':
+                self.drag_x = touch.x
+                self.drag_y = touch.y
             return True
             
     def on_touch_up(self, touch):
+        if self.mode == 'draw_connection':
+            for m in self.parent.modules:
+                if m.collide_point(touch.x,touch.y) and m != self:
+                    self.connected_modules.append(m)
+                    print m.filename
+           
         if touch.id in self.touchstarts:
             self.touchstarts.remove(touch.id)
             self.mode = 'move'
             return True
 
 
-ws = Workspace(do_rotation = False)
-s = LabIcon(filename = 'sl-addSynth+.svg')
-s2 = LabIcon(filename = 'sl-speaker.svg')
+workspace = Workspace(do_rotation = False)
+
 w = MTWindow(style = {'bg-color': (0,0,0,1)})
-ws.add_widget(s)
-ws.add_widget(s2)
-l = Line()
-ws.add_widget(l)
-w.add_widget(ws)
+w.add_widget(workspace)
 
 runTouchApp()

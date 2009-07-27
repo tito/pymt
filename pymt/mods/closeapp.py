d46240d5e3cedfd8946479cc3bc6a4e9770f9b54
Mathieu Virbel <txprog@gmail.com>
2009-07-27 11:16:38 +0200

    closeapp: new module to close any app with one finger more than 3s and no movement (< 0.02) in the top-right corner screen

---
+++ pymt/mods/closeapp.py
@@ -0,0 +1,79 @@
+'''
+Close an application with one finger
+'''
+from __future__ import with_statement
+from pymt import MTWidget, getFrameDt, getAvailableTouchs, Vector, set_color
+from pymt import stopTouchApp, gx_matrix
+from pyglet import clock
+from pyglet.gl import gluNewQuadric, gluPartialDisk, glTranslated
+
+
+# taken from nuipaint
+def drawSemiCircle(pos=(0,0), inner_radius=100, outer_radius=100, slices=32, loops=1 ,start_angle=0, sweep_angle=0):
+    with gx_matrix:
+        glTranslated(pos[0], pos[1], 0)
+        gluPartialDisk(gluNewQuadric(), inner_radius, outer_radius, slices, loops, start_angle,sweep_angle )
+
+class CloseApp(MTWidget):
+    def __init__(self, **kwargs):
+        super(CloseApp, self).__init__(**kwargs)
+        self.dt = 0
+        self.closetouches = {}
+
+    def do_close(self):
+        stopTouchApp()
+
+    def draw(self):
+        self.bring_to_front()
+        t = clock.get_default().time()
+        touches = getAvailableTouchs()
+
+        # draw closed touches
+        to_delete = []
+        ids = [touch.id for touch in touches]
+        for id in self.closetouches:
+            if not id in ids:
+                to_delete.append(id)
+                continue
+            touch = self.closetouches[id]
+            value = ((t - touch.time_start) - 3) / 2.
+            if value > 1:
+                self.do_close()
+                return
+            set_color(1, 1, 1, .7)
+            drawSemiCircle(pos=(touch.x, touch.y), inner_radius=30, outer_radius=50, slices=64, sweep_angle=value*360)
+
+        # delete old touches
+        for id in to_delete:
+            del self.closetouches[id]
+
+        # search 
+        for touch in touches:
+            if hasattr(touch, '__invalid_for_close') and touch.__invalid_for_close:
+                continue
+            # distance < 20
+            if Vector(*touch.opos).distance(Vector(touch.sx, touch.sy)) > 0.015:
+                # flag
+                touch.__invalid_for_close = True
+                if touch.id in self.closetouches:
+                    del self.closetouches[touch.id]
+                return
+            # 3s minimum
+            if t - touch.time_start < 3:
+                if touch.id in self.closetouches:
+                    del self.closetouches[touch.id]
+                return
+            # check corner screen
+            if touch.sx < .75 or touch.sy < .75:
+                if touch.id in self.closetouches:
+                    del self.closetouches[touch.id]
+                return
+            # add touches to closed touches
+            self.closetouches[touch.id] = touch
+
+def start(win, ctx):
+    ctx.w = CloseApp()
+    win.add_widget(ctx.w)
+
+def stop(win, ctx):
+    win.remove_widget(ctx.w)

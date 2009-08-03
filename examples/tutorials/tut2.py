379ee96ed99b11654b20a9cd696ddf087d85a1a5
tommy.bjorling <devnull@localhost>
2009-08-03 23:06:51 +0200

    tut2: fix for new unified-event

--- examples/tutorials/tut2.py
+++ examples/tutorials/tut2.py
@@ -10,7 +10,7 @@ l = MTLabel(label='#', font_size=200, pos=(270, 40))
 w.add_widget(l)
 
 @b.event
-def on_press(touchID, x, y):
-    l.text = str(random.randrange(0, 100))
+def on_press(touch):
+    l.label = str(random.randrange(0, 100))
 
 runTouchApp() 

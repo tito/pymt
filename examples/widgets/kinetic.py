4f876ece159801f496f7381498dbe023047754eb
Riley Dutton <riley@rileydutton.com>
2009-08-12 12:25:40 -0500

    innerwindow: fixed issue #89, added line to call parent on_draw method. Also updated kinetic.py (took out reference to file on tito's computer)

--- examples/widgets/kinetic.py
+++ examples/widgets/kinetic.py
@@ -7,17 +7,20 @@ def my_onpress_handlers(*largs):
     print 'touched'
 
 mms = MTWindow()
+w = MTInnerWindow(size=(600, 600))
+mms.add_widget(w)
 
 #k = MTKineticList(pos=(20, 20), size=(400, 400), h_limit=2, w_limit=0, do_x=True, do_y=False)
 k = MTKineticList(pos=(20, 20), size=(400, 400), w_limit=3)
-mms.add_widget(k)
+w.add_widget(k)
 
 d = range(0, 10)
 for x in d:
     item = MTKineticItem(label=str(x))
     item.push_handlers(on_press=curry(print_me, item))
     k.add_widget(item)
-item = MTKineticImage(filename='/home/tito/code/nuipaint/filters/puppy.jpg')
-item.push_handlers(on_press=my_onpress_handlers)
-k.add_widget(item)
+#item = MTKineticImage(filename='/home/tito/code/nuipaint/filters/puppy.jpg')
+#item.push_handlers(on_press=my_onpress_handlers)
+#k.add_widget(item)
+
 runTouchApp()

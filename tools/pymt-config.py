a5bdd9e4997ee73e584a8b50165001da2de4b5b8
Mathieu Virbel <txprog@gmail.com>
2009-08-29 14:15:23 +0200

    core: replace enablemouse by configuration [pymt] enable_simulator, fix __init__ & pymt-config (fixes issue 98)

--- tools/pymt-config.py
+++ tools/pymt-config.py
@@ -104,14 +104,17 @@ g_pymt.grid(row=0, column=0, sticky=W+E+N+S)
 
 Label(g_pymt, text='Show FPS').grid(row=0)
 Label(g_pymt, text='Show event stats').grid(row=1)
-Label(g_pymt, text='Log level').grid(row=2)
-Label(g_pymt, text='Double tap time').grid(row=3)
-Label(g_pymt, text='Double tap distance').grid(row=4)
+Label(g_pymt, text='Enable mouse simulator').grid(row=2)
+Label(g_pymt, text='Log level').grid(row=3)
+Label(g_pymt, text='Double tap time').grid(row=4)
+Label(g_pymt, text='Double tap distance').grid(row=5)
 
 e_pymt_fps = Checkbutton(g_pymt,
         variable=c['pymt.show_fps'], onvalue='1', offvalue='0')
 e_pymt_eventstats = Checkbutton(g_pymt,
         variable=c['pymt.show_eventstats'], onvalue='1', offvalue='0')
+e_pymt_enablesimulator = Checkbutton(g_pymt,
+        variable=c['pymt.enable_simulator'], onvalue='1', offvalue='0')
 e_pymt_loglevel = OptionMenu(g_pymt, c['pymt.log_level'], *opt_loglevel)
 e_pymt_doubletaptime = Scale(g_pymt, from_=0, to=1000, orient=HORIZONTAL,
         variable=c['pymt.double_tap_time'])
@@ -120,9 +123,10 @@ e_pymt_doubletapdistance = Scale(g_pymt, from_=0, to=300, orient=HORIZONTAL,
 
 e_pymt_fps.grid(row=0, column=1)
 e_pymt_eventstats.grid(row=1, column=1)
-e_pymt_loglevel.grid(row=2, column=1)
-e_pymt_doubletaptime.grid(row=3, column=1, sticky=W+E+N+S)
-e_pymt_doubletapdistance.grid(row=4, column=1, sticky=W+E+N+S)
+e_pymt_enablesimulator.grid(row=2, column=1)
+e_pymt_loglevel.grid(row=3, column=1)
+e_pymt_doubletaptime.grid(row=4, column=1, sticky=W+E+N+S)
+e_pymt_doubletapdistance.grid(row=5, column=1, sticky=W+E+N+S)
 
 # ================================================================
 # Graphics

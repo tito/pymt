b3d462a2ad5f8d7be85908771ee0862eaae790fa
sharath.patali <devnull@localhost>
2009-09-04 02:46:08 +0530

    Video Widget: Modified the code as per review done by tito

--- examples/videoplayer/video.py
+++ examples/videoplayer/video.py
@@ -13,17 +13,17 @@ if __name__ == '__main__':
     
     
     #normal MTVideo
-    video = MTVideo(video='super-fly.avi',pos=(0,0),on_playback_end='loop',volume=0.1)
+    video = MTVideo(filename='super-fly.avi',pos=(0,0),on_playback_end='loop',volume=0.1)
     scat = MTScatterWidget(size=video.size)
     scat.add_widget(video)    
     w.add_widget(scat)
    
-    @video.event
-    def on_playback_end():
-        print "playback ended"
+    #@video.event
+    #def on_playback_end():
+    #    print "playback ended"
     
     #Simple Video Widget
-    video2 = MTSimpleVideo(video='super-fly.avi',volume=0.1,pos=(300,200))
-    w.add_widget(video2)
+    #video2 = MTSimpleVideo(filename='super-fly.avi',volume=0.1,pos=(300,200))
+    #w.add_widget(video2)
 
     runTouchApp()

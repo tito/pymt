fe1bef767cd5b579dc5dc3e53ea5b34f2c136509
Mathieu Virbel <txprog@gmail.com>
2009-09-16 12:37:53 +0200

    finally reference counting are working. without segfault :p

--- pymtcore/src/widget.h
+++ pymtcore/src/widget.h
@@ -19,93 +19,108 @@ public:
 
     virtual ~MTWidget()
     {
-        std::cout << "ctor " << this << std::endl;
         this->clear();
     }
 
     void clear(void)
     {
-        /**
-        MTWidget *widget;
-        while ( this->children.begin() != this->children.end() )
-        {
-            std::cout << "size=" << this->children.size() << std::endl;
-            widget = this->children.back();
-            if ( widget->parent )
-                widget->parent->unref();
-            this->children.pop_back();
-        }
-        **/
+        while ( !this->children.empty()  )
+            this->remove_widget(this->children.back());
     }
 
-    int ref(void)
+    void ref(void)
     {
         /* automaticly disown the object if it's handled here
+         * we handle the reference counting ourself.
          */
         Swig::Director *director = dynamic_cast<Swig::Director *>(this);
-        std::cout << "ref(), director=" << director << " for " << this << std::endl;
 
         this->__ref_count++;
 
         if ( director )
-        {
-            std::cout << "disown called " << this << std::endl;
             director->swig_disown();
-        }
-
-        return this->__ref_count;
     }
 
-    int unref(int fromswig=0)
+    void unref(int fromswig=0)
     {
-        std::cout << "unref " << this << " from " << fromswig << std::endl;
+        /* if swig don't have anymore a reference on it,
+         * and we are orphan, clear our children list.
+         */
+        if ( fromswig && this->parent == NULL )
+            this->clear();
+
+        /* decrement the reference counting
+         */
         this->__ref_count--;
         if ( this->__ref_count <= 0 )
         {
-            std::cout << "delete this" << this << std::endl;
             delete this;
-            return 0;
+            return;
         } 
-
-        if ( fromswig )
-        {
-            this->clear();
-        }
-
-        return this->__ref_count;
     }
 
 
     virtual void add_widget(MTWidget *widget)
     {
+        // TODO add exception, a widget cannot have 2 parent !
+        if ( widget->parent != NULL )
+            return;
+
+        /* add the widget at the back
+         */
         this->children.push_back(widget);
+
+        /* reference the widget and ourself as parent
+         */
+        this->ref();
         widget->ref();
         widget->parent = this;
-        this->ref();
     }
 
     virtual void remove_widget(MTWidget *widget)
     {
+        MTWidget *parent;
         std::vector<MTWidget *>::iterator i = this->children.begin();
+
+        /* reference ourself, we don't want to be removed
+         * when a widget remove is done
+         */
+        this->ref();
+
 		for ( ; i != this->children.end(); i++ )
         {
+            /* if the widget is not found, continue
+             */
             if ( *i != widget )
                 continue;
-            (*i)->parent->unref();
+
+            /* remove the widget from the list
+             */
+            this->children.erase(i);
+
+            /* prevent recursion on unref() call
+             */
+            parent = (*i)->parent;
             (*i)->parent = NULL;
+            if ( parent != NULL )
+                parent->unref();
+
+            /* unref the children.
+             */
             (*i)->unref();
-            return;
+            break;
 		}
 
+        /* unref ourself.
+         */
+        this->unref();
     }
 
     virtual void on_update(void)
     {
         std::vector<MTWidget *>::iterator i = this->children.begin();
         for ( ; i != this->children.end(); i++ )
-        {
             (*i)->on_update();
-        }
     }
 
     virtual void on_draw(void)
@@ -115,19 +130,17 @@ public:
         this->draw();
 
         for ( ; i != this->children.end(); i++ )
-        {
             (*i)->on_draw();
-        }
     }
 
     virtual void draw(void)
     {
     }
 
-    void print_debug_internal(void)
+
+    int get_ref_count(void)
     {
-        std::cout << "Object " << this << std::endl;
-        std::cout << "* __ref_count = " << this->__ref_count << std::endl;
+        return this->__ref_count;
     }
 
     std::vector<MTWidget *> children;

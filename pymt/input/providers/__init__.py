369117cab29c38a46e5ddb9050d4a14d55f08fd6
Mathieu Virbel <txprog@gmail.com>
2009-11-16 18:44:18 +0100

    input: new mactouch provider for native usage of multitouchsupport framework in macosx (thanks a lot to dennda for testing!)

--- pymt/input/providers/__init__.py
+++ pymt/input/providers/__init__.py
@@ -2,6 +2,7 @@
 Providers: list of all input providers
 '''
 
+import pymt
 import sys
 
 from tuio import *
@@ -10,3 +11,10 @@ from mouse import *
 if sys.platform == 'win32':
     from wm_touch import *
     from wm_pen import *
+
+if sys.platform == 'darwin':
+    try:
+        from mactouch import *
+    except:
+        pymt.pymt_logger.exception('MacMultitouchSupport is not available for your version')
+

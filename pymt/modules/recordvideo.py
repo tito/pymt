d88fc07a635807a7ee9adbc650b85f3e4baa0962
Mathieu Virbel <txprog@gmail.com>
2009-12-19 11:19:09 +0100

    recordvideo: fix recordvideo by saving image in RGB mode instead of RGBA

--- pymt/modules/recordvideo.py
+++ pymt/modules/recordvideo.py
@@ -4,7 +4,7 @@ Record the opengl output into a video
 
 import pygame
 import pymt
-from OpenGL.GL import glReadBuffer, glReadPixels, GL_RGBA, GL_UNSIGNED_BYTE, GL_BACK
+from OpenGL.GL import glReadBuffer, glReadPixels, GL_RGB, GL_UNSIGNED_BYTE, GL_FRONT
 from pymt.utils import curry
 
 dump_prefix    = pymt.pymt_config.get('dump', 'prefix')
@@ -14,9 +14,9 @@ dump_idx       = 0
 def window_flip_and_save():
     global dump_idx
     win = pymt.getWindow()
-    glReadBuffer(GL_BACK)
-    data = glReadPixels(0, 0, win.width, win.height, GL_RGBA, GL_UNSIGNED_BYTE)
-    surface = pygame.image.fromstring(str(buffer(data)), win.size, 'RGBA', True)
+    glReadBuffer(GL_FRONT)
+    data = glReadPixels(0, 0, win.width, win.height, GL_RGB, GL_UNSIGNED_BYTE)
+    surface = pygame.image.fromstring(str(buffer(data)), win.size, 'RGB', True)
     filename = '%s%05d.%s' % (dump_prefix, dump_idx, dump_format)
     pygame.image.save(surface, filename)
     dump_idx += 1

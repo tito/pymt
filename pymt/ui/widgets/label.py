ca14708c7fc55b608d960d218c3e61b4b12f5041
Mathieu Virbel <txprog@gmail.com>
2010-01-07 14:13:34 +0100

    mtlabel: add autosize + fix size of label at start (depend on auto{size,width,height})

--- pymt/ui/widgets/label.py
+++ pymt/ui/widgets/label.py
@@ -6,7 +6,7 @@ Label: a simple text label
 __all__ = ['MTLabel']
 
 import pymt
-from ...graphx import drawLabel, set_color, drawCSSRectangle
+from ...graphx import drawLabel, set_color, drawCSSRectangle, getLabel
 from ..factory import MTWidgetFactory
 from widget import MTWidget
 
@@ -16,6 +16,8 @@ class MTLabel(MTWidget):
         label = MTLabel(label='Plop world')
 
     :Parameters:
+        `autosize`: bool, default to True
+            Update size information with label size
         `autowidth`: bool, default to True
             Update width information with the label content width
         `autoheight`: bool, default to True
@@ -24,13 +26,16 @@ class MTLabel(MTWidget):
     def __init__(self, **kwargs):
         kwargs.setdefault('autowidth', False)
         kwargs.setdefault('autoheight', False)
+        kwargs.setdefault('autosize', False)
         kwargs.setdefault('label', '')
 
         self.autowidth  = kwargs.get('autowidth')
         self.autoheight = kwargs.get('autoheight')
+        self.autosize   = kwargs.get('autosize')
         self.label = kwargs.get('label')
         del kwargs['autowidth']
         del kwargs['autoheight']
+        del kwargs['autosize']
         del kwargs['label']
 
         super(MTLabel, self).__init__(**kwargs)
@@ -39,16 +44,25 @@ class MTLabel(MTWidget):
             del kwargs['size']
         if 'pos' in kwargs:
             del kwargs['pos']
+
         self.kwargs = kwargs
+
+        # update this label size
+        label = getLabel(label=self.label, **self.kwargs)
+        self._update_size(*label.size)
 
     def draw(self):
         set_color(*self.style.get('bg-color'))
         drawCSSRectangle(pos=self.pos, size=self.size, style=self.style)
         w, h = drawLabel(label=self.label, pos=self.center, **self.kwargs)
-        if self.autoheight:
-            self.height = h
-        if self.autowidth:
-            self.width = w
+        self._update_size(w, h)
 
+    def _update_size(self, w, h):
+        if (self.autoheight and self.autowidth) or self.autosize:
+            self.size = (w, h)
+        elif self.autoheight:
+            self.height = h
+        elif self.autowidth:
+            self.width = w
 
 MTWidgetFactory.register('MTLabel', MTLabel)

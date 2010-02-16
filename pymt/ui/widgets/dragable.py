6608ab4684490b8ef9a221bdaac8cb98ec2eb26d
hansent <thomas.hansen@gmail.com>
2010-02-16 13:55:18 -0600

    changed ring color and small change to dragable

--- pymt/ui/widgets/dragable.py
+++ pymt/ui/widgets/dragable.py
@@ -6,27 +6,32 @@ __all__ = ['MTDragable']
 
 from ..factory import MTWidgetFactory
 from widget import MTWidget
+from pymt.vector import Vector
 
 class MTDragable(MTWidget):
     '''MTDragable is a moveable widget over the window'''
     def __init__(self, **kwargs):
         super(MTDragable, self).__init__(**kwargs)
-        self.state = ('normal', None)
+        self.state = 'normal'
 
     def on_touch_down(self, touch):
+        if self.state == 'dragging':
+            return False
         if self.collide_point(touch.x, touch.y):
-            self.state = ('dragging', touch.id, touch.x, touch.y)
+            self.state = 'dragging'
+            touch.grab(self)
+            touch.userdata['touch_offset'] = Vector(self.pos)-touch.pos
             return True
 
     def on_touch_move(self, touch):
-        if self.state[0] == 'dragging' and self.state[1] == touch.id:
-            self.x, self.y = (self.x + (touch.x - self.state[2]) , self.y + touch.y - self.state[3])
-            self.state = ('dragging', touch.id, touch.x, touch.y)
+        if touch.grab_current == self:
+            self.pos = touch.userdata['touch_offset'] + touch.pos
             return True
 
     def on_touch_up(self, touch):
-        if self.state[1] == touch.id:
-            self.state = ('normal', None)
+        if touch.grab_current == self:
+            self.state = 'normal'
+            touch.ungrab(self)
             return True
 
 MTWidgetFactory.register('MTDragable', MTDragable)

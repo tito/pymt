d231e150a4a46e7015f2d0f89923e3a5f67ee98b
Gabriel Pettier <gaby@cheshire-cat.(none)>
2010-05-04 16:31:13 +0200

    added a smass test suite for css styling

---
+++ tests/test_css.py
@@ -0,0 +1,19 @@
+'''
+Css styling basic tests
+'''
+
+from init import test, import_pymt_no_window
+
+def unittest_css():
+    import_pymt_no_window()
+    from pymt import MTWidget
+    from pymt import css_add_sheet, css_reload
+    css_add_sheet('''
+    .style { bg-color: rgba(255, 255, 255, 255);}
+    #my { bg-color: rgba(255, 0, 255, 0);}
+    ''')
+    w = MTWidget(cls='style')
+    x = MTWidget(id='my',cls='style')
+    #css_reload()
+    test(w.style['bg-color'] == [1.0 ,1.0 ,1.0 ,1.0])
+    test(x.style['bg-color'] == [1.0 ,0.0 ,1.0 ,0.0])

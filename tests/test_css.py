392794d6e99b74122e16af0883788f338f061750
Gabriel Pettier <gaby@cheshire-cat.(none)>
2010-05-05 15:14:27 +0200

    test css parsing with spaces

--- tests/test_css.py
+++ tests/test_css.py
@@ -10,7 +10,7 @@ def unittest_css():
     from pymt import css_add_sheet, css_reload
     css_add_sheet('''
     .style { bg-color: rgba(255, 255, 255, 255);}
-    #my { bg-color: rgba(255, 0, 255, 0);}
+    #my { bg-color : rgba(255, 0, 255, 0);}
     ''')
     w = MTWidget(cls='style')
     x = MTWidget(id='my',cls='style')

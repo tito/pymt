582cb33e0db5478c2c62d2affb423241cf65576a
tshirtman <gabriel.pettier@gmail.com>
2010-05-12 21:54:14 +0800

    added a test for multiple class css

--- tests/test_css.py
+++ tests/test_css.py
@@ -6,10 +6,12 @@ from init import test, import_pymt_no_window
 
 def unittest_css():
     import_pymt_no_window()
-    from pymt import MTWidget
-    from pymt import css_add_sheet, css_reload
+    from pymt import MTWidget, Label
+    from pymt import css_add_sheet
     css_add_sheet('''
-    .style { bg-color: rgba(255, 255, 255, 255);}
+    .style {
+        bg-color: rgba(255, 255, 255, 255);
+        }
     #my { bg-color : rgba(255, 0, 255, 0);}
     ''')
     w = MTWidget(cls='style')
@@ -18,3 +20,29 @@ def unittest_css():
     test(x.style['bg-color'] == [1.0 ,0.0 ,1.0 ,0.0])
     x.style['bg-color'] = [0, 0, 0, 0]
     test(x.style['bg-color'] == [0 ,0 ,0 ,0])
+
+def unittest_css_label():
+    import_pymt_no_window()
+    from pymt import MTLabel, css_add_sheet
+    css_add_sheet('''
+    .style {
+        color: rgba(0, 255, 0, 255);
+    }
+    ''')
+    l = MTLabel(label='test', cls='style')
+    test(l.style['color'] == [0.0, 1.0, 0.0, 1.0])
+    # print l.style['color'] # print [0.0, 0.0, 0.0, 1.0]
+
+def unittest_css_multiclass():
+    import_pymt_no_window()
+    from pymt import MTLabel, css_add_sheet
+    css_add_sheet('''
+    .test1 {
+        font-color : rgba(255,255,255,255);
+    }
+    .test2 {
+        font-size: 24;
+    }
+    ''')
+    l = MTLabel(label = 'test', cls=('test1', 'test2'))
+    test(l.style['font-size'] == 24)

29f1ce6d87fc90d437f589ee57bbccbf6f1342f4
Mathieu Virbel <txprog@gmail.com>
2010-05-24 17:59:18 +0200

    accelerate: better introduction

--- pymt/accelerate.py
+++ pymt/accelerate.py
@@ -1,7 +1,21 @@
 '''
-Accelerate: wrapper around _accelerate module, written in cython
+Accelerate: wrapper around _accelerate module, written in cython.
 
-This module increase internal performance of PyMT.
+This module increase internal performance of PyMT. User should not use directly
+this module. It's designed to enhance performance of:
+    * event dispatching (EventDispatcher class)
+    * event traversal (Widget class, on_update and on_draw)
+    * collide method (Widget class, collide_point)
+
+Accelerate module use cython, and is activated by default, if cython is
+correctly installed. Please refer to http://www.cython.org/ about how
+to install cython on your environment.
+
+You can control the usage of accelerate module with env variable ::
+
+    PYMT_USE_ACCELERATE
+
+If the env is set to 0, the module will be deactivated.
 '''
 
 __all__ = ('accelerate', )
@@ -18,7 +32,7 @@ if options.get('use_accelerate'):
         pyximport.install()
     except ImportError:
         pyximport = None
-        pymt_logger.warning('Core: unable to use pyximport (not installed ?')
+        pymt_logger.warning('Core: pyximport unavailable (cython not installed ?)')
         pymt_logger.exception('')
 
     if pyximport:

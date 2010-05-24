62372d42d5a253b1508775723759c8b4ef618d08
Christopher Denter <dennda@the-space-station.com>
2010-05-24 16:09:23 +0200

    core: spelling: First draft for spelling (spelling correction/word suggestion) provider + enchant backend

--- pymt/__init__.py
+++ pymt/__init__.py
@@ -45,6 +45,7 @@ options = {
     'image': ('pil', 'pygame'),
     'camera': ('opencv', 'gstreamer', 'videocapture'),
     'svg': ('squirtle',),
+    'spelling': ('enchant',),
 }
 
 # Read environment

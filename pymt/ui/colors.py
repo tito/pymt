tito/pymt

commit 391a9f60c268a4ff94034d07aa1341f2a05b5e4b

css: add css_reload, that will reload all css source, and update all known widgets. (fixes issue 224) keybinding: add F7 to reload css. examples: add cssreload.py example

pymt/ui/colors.py
@@ -24,7 +24,8 @@ please look on the widget documentation.
 
 __all__ = ['css_get_style', 'get_truncated_classname',
            'pymt_sheet', 'css_add_sheet', 'css_add_file', 'css_get_widget_id',
-           'css_register_state', 'css_add_keyword', 'css_register_prefix']
+           'css_register_state', 'css_add_keyword', 'css_register_prefix',
+           'css_reload']
 
 from ..logger import pymt_logger
 from .. import Cache
@@ -35,6 +36,7 @@ import sys
 import shutil
 import logging
 import re
+import weakref
 
 # Register CSS cache
 Cache.register('css')
@@ -48,6 +50,10 @@ pymt_css_states = ['-down', '-move', '-dragging', '-active', '-error',
 
 #: Prefix allowed to CSS rules
 pymt_css_prefix = ['key-', 'slider-', 'title-']
+
+# Privates vars for reload features
+_css_sources = []
+_css_widgets = []
 
 # Auto conversion from css to a special type.
 css_keyword_convert = {
@@ -107,6 +113,12 @@ css_keyword_convert = {
 
 class CSSSheet(object):
     def __init__(self):
+        self._rule = ''
+        self._content = ''
+        self._state = 'rule'
+        self._css = {}
+
+    def reset(self):
         self._rule = ''
         self._content = ''
         self._state = 'rule'
@@ -286,6 +298,11 @@ def css_get_style(widget):
     '''
 
     global pymt_sheet
+
+    ref = weakref.ref(widget)
+    if not ref in _css_widgets:
+        _css_widgets.append(ref)
+
     idwidget = css_get_widget_id(widget)
     styles = Cache.get('css', idwidget)
     if styles is not None:
@@ -295,14 +312,16 @@ def css_get_style(widget):
     Cache.append('css', idwidget, styles)
     return styles
 
-def css_add_sheet(text):
+def css_add_sheet(text, _reload=False):
     '''Add a css text to use ::
         mycss = '#buttonA { bg-color: rgba(255, 127, 0, 127); }'
         css_add_sheet(mycss)
     '''
     pymt_sheet.parse_text(text)
+    if not _reload:
+        _css_sources.append((css_add_sheet, (text, )))
 
-def css_add_file(cssfile):
+def css_add_file(cssfile, _reload=False):
     '''Add a css file to use.
     Adds all the css rules in the given file to the pymt css rule set being
     used ::
@@ -310,6 +329,8 @@ def css_add_file(cssfile):
     '''
     with open(cssfile, 'r') as fd:
         pymt_sheet.parse_text(fd.read())
+    if not _reload:
+        _css_sources.append((css_add_file, (cssfile, )))
 
 def css_register_state(name):
     '''Register a new state'''
@@ -324,6 +345,19 @@ def css_add_keyword(keyword, convertfunc):
     Convert function can be found in parser.py'''
     css_keyword_convert[keyword] = convertfunc
 
+def css_reload():
+    pymt_logger.debug('CSS: Reloading CSS in progress')
+    pymt_sheet.reset()
+    for callback, args in _css_sources[:]:
+        callback(*args, _reload=True)
+    Cache.remove('css')
+    for r in _css_widgets[:]:
+        o = r()
+        if o is None:
+            _css_widgets.remove(o)
+            continue
+        o.reload_css(css_get_style(o))
+    pymt_logger.info('CSS: CSS Reloaded')
 
 # Autoload the default css + user css
 if 'PYMT_DOC' not in os.environ:

examples/widgets/cssreload.py
@@ -1,0 +1,16 @@
+from pymt import *
+
+css_add_file('reload.css')
+
+b1 = MTButton(id='btn1', label='Reload CSS', pos=(100, 100))
+b2 = MTButton(id='btn2', label='Button 2', pos=(250, 100))
+
+@b1.event
+def on_press(*largs):
+    css_reload()
+
+w = getWindow()
+w.add_widget(b1)
+w.add_widget(b2)
+
+runTouchApp()

examples/widgets/reload.css
@@ -1,0 +1,11 @@
+/**
+ * Change this CSS and hit F7 or reload button
+ * on cssreload.py example
+ */
+#btn1 {
+	border-color: rgb(255, 0, 0);
+	draw-background: 0;
+	draw-border: 1;
+	draw-text-shadow: 1;
+	color: rgb(0,0,0);
+}

pymt/modules/keybinding.py
@@ -9,6 +9,7 @@ import logging
 from pymt.base import getWindow
 from pymt.graphx import drawRectangle, drawLabel, set_color, drawLine, drawCircle
 from pymt.logger import pymt_logger_history, pymt_logger
+from pymt.ui.colors import css_reload
 
 _toggle_state = ''
 
@@ -243,6 +244,8 @@ def _on_keyboard_handler(key, scancode, unicode):
         win.toggle_fullscreen()
     elif key == 287: # F6
         toggle('log')
+    elif key == 288: # F7
+        css_reload()
     elif key == 293:
         _screenshot()
 

pymt/ui/widgets/label.py
@@ -49,6 +49,8 @@ class MTLabel(MTWidget):
         del kwargs['autosize']
         del kwargs['label']
 
+        self.kwargs = {}
+
         super(MTLabel, self).__init__(**kwargs)
 
         size_specified = 'size' in kwargs or 'width' in kwargs or 'height' in kwargs
@@ -58,21 +60,47 @@ class MTLabel(MTWidget):
 
         self.kwargs = kwargs
 
-        #apply default CSS if not already set in kwargs
-        self.kwargs.setdefault('color', self.style.get('color'))
-        self.kwargs.setdefault('font_name', self.style.get('font-name'))
-        self.kwargs.setdefault('font_size', self.style.get('font-size'))
-        if self.style.get('font-weight') in ['bold', 'bolditalic']:
-            self.kwargs.setdefault('bold', True)
-        if self.style.get('font-weight') in ['italic', 'bolditalic']:
-            self.kwargs.setdefault('italic', True)
+        # copy style to inline one (needed for css reloading)
+        if 'color' in kwargs:
+            self._inline_style['color'] = kwargs.get('color')
+        if 'font-name' in kwargs:
+            self._inline_style['font-name'] = kwargs.get('font-name')
+        if 'font-size' in kwargs:
+            self._inline_style['font-size'] = kwargs.get('font-size')
+        if 'bold' in kwargs and 'italic' in kwargs and \
+            kwargs.get('bold') and kwargs.get('italic'):
+            self._inline_style['font-weight'] = 'bolditalic'
+        elif 'bold' in kwargs and kwargs.get('bold'):
+            self._inline_style['font-weight'] = 'bold'
+        elif 'italic' in kwargs and kwargs.get('italic'):
+            self._inline_style['font-weight'] = 'italic'
+
+        # update from inline
+        self.apply_css(self._inline_style)
 
         # update this label size
-        label = getLabel(label=self.label, **self.kwargs)
+        label = getLabel(label=self.label, **kwargs)
         if not size_specified:
             self.size = label.size
         self._update_size(*label.size)
         self._used_label = label
+
+    def apply_css(self, styles):
+        super(MTLabel, self).apply_css(styles)
+
+        # transform css attribute to style one
+        k = self.kwargs
+        s = self.style
+        k['color'] = s['color']
+        k['font_name'] = s['font-name']
+        k['font_size'] = s['font-size']
+        k['bold'] = False
+        k['italic'] = False
+        if s['font-weight'] in ('bold', 'bolditalic'):
+            k['bold'] = True
+        if s['font-weight'] in ('italic', 'bolditalic'):
+            k['italic'] = True
+
 
     @property
     def label_obj(self):

pymt/ui/widgets/widget.py
@@ -160,6 +160,7 @@ class MTWidget(EventDispatcher):
             self.apply_css(style)
 
         # apply inline css
+        self._inline_style = kwargs.get('style')
         if len(kwargs.get('style')):
             self.apply_css(kwargs.get('style'))
 
@@ -272,6 +273,13 @@ class MTWidget(EventDispatcher):
         '''Called at __init__ time to applied css attribute in current class.
         '''
         self.style.update(styles)
+
+    def reload_css(self, styles):
+        '''Called when css want to be reloaded from scratch'''
+        self.style = {}
+        self.apply_css(styles)
+        if len(self._inline_style):
+            self.apply_css(self._inline_style)
 
     def to_widget(self, x, y):
         '''Return the coordinate from window to local widget'''

pymt/ui/window/__init__.py
@@ -96,6 +96,7 @@ class BaseWindow(EventDispatcher):
         self.apply_css(style)
 
         # apply inline css
+        self._inline_style = kwargs.get('style')
         if len(kwargs.get('style')):
             self.apply_css(kwargs.get('style'))
 
@@ -177,6 +178,12 @@ class BaseWindow(EventDispatcher):
 
     def apply_css(self, styles):
         self.style.update(styles)
+
+    def reload_css(self, styles):
+        self.style = {}
+        self.apply_css(styles)
+        if len(self._inline_style):
+            self.apply_css(self._inline_style)
 
     def _get_modifiers(self):
         return self._modifiers

commit 4b458398a93d4766984db50a85c350eb98199bea

performance: Fix speed regression with creation of MTWidgets that was introduced with 391a9f6. Use set() instead of list.

pymt/ui/colors.py
@@ -53,7 +53,7 @@ pymt_css_prefix = ['key-', 'slider-', 'title-']
 
 # Privates vars for reload features
 _css_sources = []
-_css_widgets = []
+_css_widgets = set()
 
 # Auto conversion from css to a special type.
 css_keyword_convert = {
@@ -291,7 +291,7 @@ def css_get_style(widget):
 
     ref = weakref.ref(widget)
     if not ref in _css_widgets:
-        _css_widgets.append(ref)
+        _css_widgets.add(ref)
 
     idwidget = css_get_widget_id(widget)
     styles = Cache.get('pymt.css', idwidget)
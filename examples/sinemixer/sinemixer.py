379978e7d05103857c7ac80bcbc54eb8a85cebf0
Nathanaël Lécaudé <nat@Immodium-Ubuntu.(none)>
2010-06-25 06:43:05 +0800

    examples: modifications requested by tito review

--- examples/sinemixer/sinemixer.py
+++ examples/sinemixer/sinemixer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Note: To use this example, you will need Csound and ounk.
-# Csound download: http://sourceforge.net/project/showfiles.php?group_id=81968 (take the one labeled 'f' and don't install python support if asked)
-# Ounk download: http://code.google.com/p/ounk/source/checkout (once you checkout, run setup.py)
+# Note: To use this example, you will need the pyo library.
+# Download link : http://pyo.googlecode.com
+
 
 # PYMT Plugin integration
 IS_PYMT_PLUGIN = True
@@ -12,16 +12,16 @@ PLUGIN_DESCRIPTION = 'This plugin is a demonstration of the integration between 
 
 from pymt import *
 from OpenGL.GL import *
-from pyo import *
+import pyo
 
 def pymt_plugin_activate(w, ctx):
     ctx.c = MTWidget()
     
     # We initialize the pyo server.
-    s = Server(nchnls = 2).boot()
+    s = pyo.Server(nchnls = 2).boot()
     s.start()
     
-    widget_size = (50, w.size[1] / 2)
+    widget_size = (w.size[0]/25, w.size[1] / 2)
     
     # We create 4 lists which will contain our sliders (to control pitch), buttons (to trigger the sound), pyo sine waves and fader objects.
     sliders = []
@@ -29,17 +29,24 @@ def pymt_plugin_activate(w, ctx):
     sines = []
     faders = []
     
+    vlayouts = []
+    
+    hlayout = MTBoxLayout(spacing = w.size[0] / 5)
+    hlayout.pos = (w.size[0] * 0.20, w.size[1] * 0.20)
+    
     # We create 4 instances of each of the above
     for widget in range(4):
-        sliders.append(MTSlider(min = 100, max = 1000, pos = (w.size[0]*((widget + 1)/5.) - widget_size[0]/2., 90), size = widget_size))
-        buttons.append(MTButton(label = '', pos = (w.size[0]*((widget + 1)/5.) - widget_size[0]/2., 30), size = (widget_size[0], widget_size[0])))
-        faders.append(Fader(fadein = 0.5, fadeout = 0.5, dur = 0, mul = 0.25))
-        sines.append(Sine(mul = faders[widget], freq = 100))
+        vlayouts.append(MTBoxLayout(orientation = 'vertical', spacing = 10))
+        sliders.append(MTSlider(min = 100, max = 1000, size = widget_size))
+        buttons.append(MTButton(label = '', size = (widget_size[0], widget_size[0])))
+        vlayouts[widget].add_widget(buttons[widget])
+        vlayouts[widget].add_widget(sliders[widget])
+        hlayout.add_widget(vlayouts[widget])
+        faders.append(pyo.Fader(fadein = 0.5, fadeout = 0.5, dur = 0, mul = 0.25))
+        sines.append(pyo.Sine(mul = faders[widget], freq = 300))
         sines[widget].out()
-        ctx.c.add_widget(sliders[widget])
-        ctx.c.add_widget(buttons[widget])
-    s1,s2,s3,s4 = sliders
-    b1,b2,b3,b4 = buttons
+        
+    ctx.c.add_widget(hlayout)
 
     # This function gets called when a slider moves, it sets the pitch of each sine.
     def on_value_change_callback(slider, value):
@@ -56,7 +63,6 @@ def pymt_plugin_activate(w, ctx):
     # We push the handlers and feed it with the slider number so the callback function knows which sine to work on.
     for s in range(4):
         sliders[s].push_handlers(on_value_change = curry(on_value_change_callback, s))
-        time.sleep(2)
         sliders[s].value = 300
         
     # Handlers for the buttons are pushed here.
@@ -67,6 +73,8 @@ def pymt_plugin_activate(w, ctx):
     w.add_widget(ctx.c)
 
 def pymt_plugin_deactivate(w, ctx):
+    # pyo Server is stopped
+    s.stop()
     w.remove_widget(ctx.c)
 
 if __name__ == '__main__':

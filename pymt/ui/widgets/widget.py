3c43c12b4bf70b174a2b82a61a3a193b266c8c10
Mathieu Virbel <txprog@gmail.com>
2010-07-01 14:31:36 +0200

    widget: if on_update is not on visible_event... register it anyway !!! (otherwise, on_update will be never executed anymore)

--- pymt/ui/widgets/widget.py
+++ pymt/ui/widgets/widget.py
@@ -133,6 +133,7 @@ class MTWidget(EventDispatcher):
         self._root_window           = None
         self._root_window_source    = None
 
+        self.register_event_type('on_update')
         self.register_event_type('on_animation_complete')
         self.register_event_type('on_resize')
         self.register_event_type('on_parent_resize')

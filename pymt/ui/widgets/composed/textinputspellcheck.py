b583135e35016ad8ca3ba4c275996eb7b58e9896
Christopher Denter <dennda -O- the-space-station -o- com>
2010-07-04 18:29:45 +0200

    spellchecking: textinputspellcheck WIP.

--- pymt/ui/widgets/composed/textinputspellcheck.py
+++ pymt/ui/widgets/composed/textinputspellcheck.py
@@ -5,9 +5,13 @@ TextInputSpellCheck: One-line textinput widget with added spelling correction
 
 __all__ = ['MTTextInputSpellCheck']
 
+from pymt.utils import curry
 from pymt.graphx import getLabel, set_color, drawLine, drawLabel
 from pymt.core.spelling import Spelling
+from pymt.ui.widgets import MTButton
 from pymt.ui.widgets.composed import MTTextInput
+from pymt.ui.widgets.klist import MTList
+from pymt.ui.widgets.layout.boxlayout import MTBoxLayout
 
 
 class MTTextInputSpellCheck(MTTextInput):
@@ -16,6 +20,7 @@ class MTTextInputSpellCheck(MTTextInput):
         self.spelling = spelling
         self.auto_correct = auto_correct
         self.connect('on_text_change', self.check_text)
+        self.words = []
         self.checked = []
         self.widths = []
         self.lines = []
@@ -26,9 +31,9 @@ class MTTextInputSpellCheck(MTTextInput):
         if self.password:
             return
         # XXX should be all whitespace chars, no?
-        words = self.value.split(" ")
+        self.words = self.value.split(" ")
         c = self.spelling.check
-        self.checked = [c(w) for w in words]
+        self.checked = [c(w) for w in self.words]
 
         # We're not using a monospace font, hence we need to
         # get the widths of the words to draw red lines below
@@ -37,8 +42,16 @@ class MTTextInputSpellCheck(MTTextInput):
         sep = getLabel(' ', font_size=font_size, **self.style)
         self._sep_width = sep.width
         self._sep_height = sep.height
-        self.widths = [getLabel(w, font_size=font_size, **self.style).width for w in words]
+        self.widths = [getLabel(w, font_size=font_size, **self.style).width for w in self.words]
 
+        cur_x, y = self.get_text_pos()
+        self.lines = []
+        for valid, width in zip(self.checked, self.widths):
+            if not valid and valid is not None:
+                self.lines.append((cur_x, y, cur_x + width, y))
+            cur_x += self._sep_width + width
+
+    def get_text_pos(self):
         pos = list(self.center)
         if self.anchor_x == 'left':
             pos[0] = self.x
@@ -49,13 +62,48 @@ class MTTextInputSpellCheck(MTTextInput):
         elif self.anchor_y == 'bottom':
             pos[1] = self.y
 
-        cur_x, y = pos
+        x, y = pos
         y -= self._sep_height / 2
-        self.lines = []
-        for valid, width in zip(self.checked, self.widths):
-            if not valid and valid is not None:
-                self.lines.append((cur_x, y, cur_x + width, y))
-            cur_x += self._sep_width + width
+        return x, y
+
+    def on_touch_down(self, touch):
+        # XXX missing self.collide_point
+        touch.userdata[MTTextInputSpellCheck] = self
+        super(MTTextInputSpellCheck, self).on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        # XXX missing self.collide_point
+        was_in = touch.userdata.get(MTTextInputSpellCheck) is self
+        if was_in:
+            # Calculate the word that was tapped
+            x, y = touch.pos
+            tx, ty = self.get_text_pos()
+            if ty <= y <= ty + self._sep_height and \
+               tx <= x <= sum(self.widths) + (len(self.widths)-1) * self._sep_width:
+                cur_x = tx
+                for index, width in enumerate(self.widths):
+                    cur_x += width
+                    if x <= cur_x:
+                        # Found what we're looking for.
+                        break
+                    cur_x += self._sep_width
+
+                # OK, set up the list of word suggestions!
+                layout = MTBoxLayout(orientation='vertical')
+                suggestions = self.spelling.suggest(self.words[index])
+                btns = [MTButton(label=s, auto_height=True) for s in suggestions]
+                layout.add_widgets(*btns)
+                print btns
+                lst = MTList(pos=(200, 200), size=(200, 500), do_y=True)
+                lst.add_widget(layout)
+                for btn in btns:
+                    btn.connect('on_press', curry(self.replace_word, index, btn.label, lst))
+                self.add_widget(lst)
+            return True
+        super(MTTextInputSpellCheck, self).on_touch_up(touch)
+
+    def replace_word(self, *args):
+        print args
 
     def draw(self):
         super(MTTextInputSpellCheck, self).draw()

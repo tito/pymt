cdd4add58a2775b37571d7ad52805956581f99de
Mathieu Virbel <txprog@gmail.com>
2010-07-19 11:05:53 +0200

    fix for easy_install

--- examples/framework/ui_widgets_slider_multi.py
+++ examples/framework/ui_widgets_slider_multi.py
@@ -4,6 +4,6 @@ sl = MTMultiSlider(init_value=0.1)
 
 @sl.event
 def on_value_change(values):
-    print 'Slider values change: ', values)
+    print 'Slider values change: ', values
 
 runTouchApp(sl)

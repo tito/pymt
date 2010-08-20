31f8b89c1b7c9de6b6a6d95dfcb4930986968689
Mathieu Virbel <mathieu@pymt.eu>
2010-08-21 00:37:16 +0200

    layout: add abstract layout accessible to the user, or he can't create his own layout too.

--- pymt/ui/widgets/layout/__init__.py
+++ pymt/ui/widgets/layout/__init__.py
@@ -2,6 +2,7 @@
 Layout: arrange widget in a layout
 '''
 
+from pymt.ui.widgets.layout.abstractlayout import MTAbstractLayout
 from pymt.ui.widgets.layout.boxlayout import MTBoxLayout
 from pymt.ui.widgets.layout.gridlayout import MTGridLayout
 from pymt.ui.widgets.layout.anchorlayout import MTAnchorLayout

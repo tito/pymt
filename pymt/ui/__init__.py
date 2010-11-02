4c5221164bc08b784e95ba0f4cb25997f6c33b76
Mathieu Virbel <mathieu@pymt.eu>
2010-11-02 16:11:43 -0400

    core: remove ui and graphx package.

--- pymt/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-UI package: base for all ui things
-'''
-from pymt.ui.factory import *
-from pymt.ui.animation import *
-from pymt.ui.window import *
-from pymt.ui.widgets import *
-from pymt.ui.colors import *
--- pymt/ui/widgets/layout/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Layout: arrange widget in a layout
-'''
-
-from pymt.ui.widgets.layout.abstractlayout import MTAbstractLayout
-from pymt.ui.widgets.layout.boxlayout import MTBoxLayout
-from pymt.ui.widgets.layout.gridlayout import MTGridLayout
-from pymt.ui.widgets.layout.anchorlayout import MTAnchorLayout
-from pymt.ui.widgets.layout.screenlayout import MTScreenLayout
